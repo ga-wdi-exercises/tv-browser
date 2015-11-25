3b4f4ab1d95b8257495f6549a8b6a67f99908c89
Eric Stennett <estennett@gmail.com>
2015-11-25 12:10:58 -0500

    basic functionality working

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,25 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+$(document).ready(function(){
+    $('#movie-select').hide();
+    $('#submit').on("click", function(event){
+      event.preventDefault();
+      var movieSearch = $("#movie-search").val();
+      var url = "http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&r=json"
+      $.ajax({
+        url: url,
+        type: "get",
+        dataType: "json"
+      }).done(function(response){
+        $('#movie-select').append('<option value="' + response.Title + '">' + response.Title + ' </option>');
+        $('#movie-select').show();
+        console.log(response)
+        $('#movie-select').on("change", function(){
+          $('#movie-detail').append('<p>' + response.Title  + '</p>')
+        })
+      }).fail(function(){
+        console.log("Ajax request fails!")
+    })
+  })
+})
