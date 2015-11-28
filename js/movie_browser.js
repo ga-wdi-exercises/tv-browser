ba50a3507919a6aa717d14195f3aa800420111eb
Tory Burgett <vdburgett@gmail.com>
2015-11-28 18:48:06 -0500

    able to search through omdb and put title resutls up

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -3,23 +3,55 @@
 
 $(document).ready(function(){
 
-  $(":submit").on("click", function(){
+  $("#movie-select").hide();
+
+  function searchIMDb(searchTerm){
     event.preventDefault();
-    var searchTerm = $("#movie-search").val();
-    $("h1").text(searchTerm);
-    // var url = "http://www.omdbapi.com/?";
-    // $.ajax({
-    //   url: url,
-    //   type: "get",
-    //   dataType: "json"
-    // }).done(function( response ){
-    //   var windPower = response.current_observation.wind_mph;
-    //   $("h1").text("WIND MPH: "+windPower+"!");sa
-    // }).fail(function(){
-    //   console.log("Ajax request fails!");
-    // }).always(function(){
-    //   console.log("This always happens regardless of successful ajax request or not.");
-    // });
+
+    $("#searchingFor").text("Results for  \'"+searchTerm+"\'...");
+
+    $("#movie-select").show();
+
+    var url = "http://www.omdbapi.com/?s="+escape(searchTerm);
+
+    $.ajax({
+      url: url,
+      dataType: "json",
+      // success: function(data) {
+      //   console.log(data);
+      //   for (i=0; i< data.Search.length; i++){
+      //     var searchResult = data.Search[i];
+      //     $("#movie-detail").append("<p> Movie "+i+", Title: "+searchResult.Title+"</p>");
+      //   }
+      //   }
+    }).done(function(data){
+      console.log(data);
+      $("#searchResults").remove();
+      $("#movie-detail").after("<div id=\"searchResults\"></div>");
+      for (i=0; i< data.Search.length; i++){
+        var searchResult = data.Search[i];
+        $("#searchResults").append("<p> Movie "+(i+1)+", Title: "+searchResult.Title+"</p>");
+      }
+
+      // for (var i=0; i< url.Search.length(); i++ ){
+      //   var searchResult = url.Search[i];
+      //   $("movie-detail").append("<p>"+searchResult.Title+"</p>");
+      //
+      //
+      //  var display = '<option value="">Movies matching "'+ searchTerm +'"...</option>';
+      // display += ['<option value="', movie.imdbID, '">', movie.Title, '</option>'].join('');
+      // }
+    }).fail(function(){
+      console.log("Ajax request fails!");
+    }).always(function(){
+      console.log("This always happens regardless of successful ajax request or not.");
+    });
+  }
+
+  $("#search").append("<div id=\"searchingFor\"></div>");
+  $(":submit").on("click", function(){
+    searchTerm = $("#movie-search").val();
+    searchIMDb(searchTerm);
   });
-  
+
 });
