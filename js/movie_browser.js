f250df1d082e2f038bd0f8da9ecd1bfcd89d036c
Armani Hall <MRBOJANGLES226@GMAIL.COM>
2015-11-29 23:34:31 -0500

    attempt

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,43 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+$("document").ready(function(){
+  $("#search").on("submit", function(e){
+    e.preventDefault();
+    getAPI($("#movie-search").val());
+  })
+})
+
+function getAPI(keyword){
+  type: "get",
+    dataType: "json"
+  }).done(function(response){
+    $("#movie-select").children().remove()
+
+  $("<option value=''>Results for: "+keyword+"</option>").appendTo($("#movie-select"))
+    for (var i=0; i<response.Search.length; i++){
+      $("#movie-select").append($('<option id='+response.Search[i].imdbID+'>'+response.Search[i].Title+'</option>'))
+    }
+    addListener();
+//show the dropdown menu
+    $("#movie-select").show();
+  })
+}
+function addListener(){
+//check for a change in the select menu
+  $("#movie-select").change("submit", function(e){
+    if ($('select option:selected').attr('id')) {
+      $.ajax({
+        url:"http://www.omdbapi.com/?i="+$('select option:selected'  ).attr('id')+"&y=&plot=short&r=json",
+        type:"get",
+        dataType: "json"
+      }).done(function(response){
+        $("#movie-detail").empty()
+        $("#movie-detail").append($("<img src="+response.Poster+"/>"))
+        $("#movie-detail").append($("<p>Title: "+response.Title+"</p>"+
+        "<p>Director: "+response.Director+"</p>"+
+        "<p>Actors: "+response.Actors+"</p>"+
+        "<p>Country: "+response.Country+"</p>"))
+      })
+    }
+  })
+}
