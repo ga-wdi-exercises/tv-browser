466b781aa981fccfcf189ba53cf4bf681034bfb3
Thomas Clark <tclark@soteriagiving.com>
2015-12-30 13:44:08 -0500

    first commit

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,17 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+
+function search (keyworkd) {
+  var url = 'http://www.omdbapi.com/?s='+escape(keyword);
+
+$.getJSON(url)
+.done(function(imdbResponse){
+
+  imdbDone(keyword, imdbResponse);
+});
+
+.fail(function(imdbResponse, textStatus, errorMessage){
+  
+})
+}
