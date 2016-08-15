f5b3507613d6baa38618c53416ddd0fdd9c4ca1c
Taylor Edwards <tedwards1884@gmail.com>
2016-08-15 17:01:04 -0400

    ajax; first select tag

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -3,12 +3,29 @@
 
 $(document).ready(function(){
 
+  $("#movie-select").hide();
+
   $("#search").submit(function(evt){
     evt.preventDefault();
     var query = $("#movie-search").val();
-    console.log(query);
-
-
+    var url = "http://www.omdbapi.com/?type=movie&s=" + query.split(" ").join("+");
+    $.ajax({
+      url: url,
+      type: "get",
+      dataType: "json"
+    }).done(function(response){
+      $("#movie-select").show().empty().append($("<option>Movies matching " + query + "...</option>"));
+      console.log(response)
+      // for(i=0; i<response.length; i++){
+      //   $("<option value = \"></option>")
+      //   $("#movie-select").append
+      //
+      // }
+    }).fail(function(){
+      console.log("bad request broh")
+    }).always(function(){
+      // console.log("still workin broh")
+    });
   })
 
 })
