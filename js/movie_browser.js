5477f35bf3984abf3d9b2097f69e5a19bc44b734
Megan Coyle <meg@mcoyle.com>
2016-08-15 17:08:53 -0400

    Initial set-up

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,25 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+$(document).ready(function() {
+  var $movieSelect = $("#movie-select");
+  var $form = $("#search");
+
+  // movie select hidden by default
+  $movieSelect.hide();
+
+    $form.on("submit", function(e) {
+      e.preventDefault();
+      // reveal movie select
+       $movieSelect.show();
+    $.ajax({
+      type: "get",
+      dataType: "json",
+      url: "http://www.omdbapi.com/?s=Ground&r=json"
+      }).done(function(res){
+        console.log(res)
+      }).fail(function(res) {
+          console.log("eeep, errors!");
+      })
+  })
+
+});
