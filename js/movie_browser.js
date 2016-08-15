5783888d329d4a18c93648fee8924f071d516dc1
Taylor Edwards <tedwards1884@gmail.com>
2016-08-15 16:38:26 -0400

    initial commit; submit listener added

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,14 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+$(document).ready(function(){
+
+  $("#search").submit(function(evt){
+    evt.preventDefault();
+    var query = $("#movie-search").val();
+    console.log(query);
+
+
+  })
+
+})
