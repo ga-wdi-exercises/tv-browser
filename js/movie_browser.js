18663632b42c3a6387b3b8ce6179337b03b6a8d6
Megan Coyle <meg@mcoyle.com>
2016-08-16 09:11:53 -0400

    Fix to remove one of the two ajax calls

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -25,28 +25,21 @@ $(document).ready(function() {
       }).done(function(res){
         $movieSelect.append("<option value='' disabled selected>Movies matching '" + $keyword +"'</option>")
         for (i = 0; i < res.Search.length; i++) {;
-        var option = "<option value='" + res.Search[i].Title + "'>" + res.Search[i].Title + "</option>";
+        var option = $("<option value='" + i + "'>" + res.Search[i].Title + "</option>");
         $movieSelect.append(option)
       };
-      $movieSelect.on("change", function() {
+      $movieSelect.change(function() {
         $details.empty();
-        var movieSelected = this.value;
-
-        $.ajax({
-         type: "get",
-         dataType: "json",
-         url: "http://www.omdbapi.com/?t=" + movieSelected
-         }).done(function(res){
-           // logic for display if poster image url exists or is n/a
-           var poster = res.Poster
-           if (poster === 'N/A') {
-             poster = ''
-           } else {
-             poster = "<img src='" + poster + "'/>"
-           }
-           var showDetails = "<h1>" + res.Title + "</h1><img src='" + res.Poster + "'/>" + "<p>" + res.Actors + "</p>" + "<p>" + res.Plot + "</p>"
-           $details.append("<h1>" + res.Title + "</h1>" + poster + "<p>" + res.Actors + "</p>" + "<p>" + res.Plot + "</p>");
-         });
+        var $selectedIndex = $(this).val();
+        var selectedMovie = res.Search[$selectedIndex];
+        var poster = selectedMovie.Poster
+        if (poster === 'N/A') {
+          poster = ''
+        } else {
+          poster = "<img src='" + poster + "'/>"
+        }
+        var showDetails = "<h1>" + selectedMovie.Title + "</h1>" + poster + "<p>" + selectedMovie.Year + "</p>";
+        $details.append(showDetails);
       })
 
       }).fail(function(res) {
