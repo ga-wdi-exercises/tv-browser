4ea4322c9d484f3845d94f9386a22f405b9fe76f
Christopher Phillips <cphillips918@gmail.com>
2016-08-16 01:49:24 -0400

    It all works!

--- js/movieView.js
+++ js/movieView.js
@@ -7,6 +7,7 @@ function SearchView(searchTerms){
 SearchView.prototype= {
     render: function(searchTerms){
         $(this.el).removeAttr('hidden')
+        $(this.el).append('<option>Options Matching: '+$('#movie-search').val()+'</option>');
         for (i=0;i<searchTerms.response.length;i++){
             $(this.el).append('<option value='+ i +'>'+searchTerms.response[i].Title +'</option>');
         }
@@ -14,6 +15,7 @@ SearchView.prototype= {
             return (sel.value)
         }
         $(this.el).change(function(){
+            $('#movie-detail').empty()
             var yas = getVal(this)
             $('#movie-detail').append('<h1>'+searchTerms.response[yas].Title+'</h1>')
             $('#movie-detail').append('<img src='+searchTerms.response[yas].Poster+' alt='+searchTerms.response[yas].Type+' />')
