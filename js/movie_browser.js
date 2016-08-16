1930901774e9c951b4c26b69146831264c7e5580
michaelmahony <michael.mahony@umbc.edu>
2016-08-16 09:37:40 -0400

    Now selected movies have their details shown.

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -11,11 +11,16 @@ $(document).ready(function () {
         if (this.type == 'submit') {
             event.preventDefault();
 
-            $("#movie-select").css('visibility', 'visible');
-
             var search_query = $("#movie-search").val();
             console.log(search_query);
-            getMovies(search_query)
+
+            $("#movie-select").empty();
+            $("#movie-select").append("<option value=''>Movies matching " + search_query + "...</option>")
+            $("#movie-select").css('visibility', 'visible');
+
+            getMovies(search_query);
+
+
         }
     })
     
@@ -50,5 +55,33 @@ $(document).ready(function () {
         $("#movie-select").append(option);
     }
 
-    
+    $('#movie-select').on('change', function() {
+        // alert( this.value ); // or $(this).val()
+        getMovie(this.value)
+    });
+
+    function getMovie(imdbID) {
+        // Set up the request
+        var url = "http://www.omdbapi.com/?i=" + imdbID;
+        var request = $.ajax({
+            url: url,
+            type: "get",
+            dataType: "json"
+        }).done(function(response){
+            console.log(request);
+            console.log(response);
+            displayMovie(response)
+
+        }).fail(function(){
+            console.log("ajax request failed");
+        }).always(function(){
+            console.log("this always happen regardless of success of failure");
+        })
+    }
 })
+
+    function displayMovie(movieObject) {
+        $("#movie-title").html(movieObject.Title);
+        $("#movie-img").attr('src', movieObject.Poster);
+        $("#movie-description").html(movieObject.Plot);
+    }
