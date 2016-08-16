c06d52d5f9435dca350bedb5b5aeb9a05f516b3b
Tyler Rosenberg <tyryanr@gmail.com>
2016-08-15 20:43:29 -0400

    got dropdown movie bar to display on submit button click

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,31 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+
+$(document).ready(function(evt) {
+
+  $("input[type=submit]").on("click", function(evt) {
+    evt.preventDefault();
+
+    $("#movie-select").css("visibility", "visible");
+
+    var searchInput = $("#movie-search").val();
+    searchMovies(searchInput);
+    function searchMovies(searchInput) {
+      $.ajax({
+        url: 'http://www.omdbapi.com/?s='+searchInput+'',
+        type: "get",
+        dataType: "json"
+      }).done(function() {
+        console.log("works");
+      }).fail(function() {
+
+      }).always(function() {
+
+      })
+    }
+
+  })
+})
+
+// http://www.omdbapi.com/?s=
