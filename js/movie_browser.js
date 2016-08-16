6091518dbe0f44f36ac031ee4b18e0dedc60b726
pamelaolney <pjolney12@gmail.com>
2016-08-16 06:43:39 -0400

    completed hw

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,26 @@
-// API Docs at: 
-// http://www.omdbapi.com
+function searchMovie(search){
+  $.ajax({
+    url: "http://www.omdbapi.com/?s="+search,
+    type: "GET",
+    dataType: "json"
+  }).done(function(response){
+    $("#first-option").html('<option> Movies matching: '+ search + '</option>')
+    for (var i=0; i < response.Search.length; i++){
+      $("#movie-select").show().append('<option value="'+response.Search[i].Title+'">' + response.Search[i].Title + '</option>')
+    }
+    console.log(response)
+  }).fail(function(){
+    console.log("this failed")
+  }).always(function(){
+    console.log("this always passes");
+  })
+}
+
+$(document).ready(function(){
+  $('#movie-select').hide()
+  $("#search").on("click", function(evt){
+    evt.preventDefault();
+    var movieSearch =  $("#movie-search").val()
+    searchMovie(movieSearch)
+  })
+})
