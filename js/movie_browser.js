4813f691216e90d3cd5b3f3cb4c0601d226f04ef
Khoi Le <khoileee89@gmail.com>
2016-10-24 19:14:14 -0400

    drop down bar populates

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,25 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+$("#movie-select").hide()
+
+$(".submit").on("click", () =>{
+  let search = $("#movie-search").val()
+  $("#movie-select").show()
+  const movieApi = "http://www.omdbapi.com/?s=" + search
+  $.ajax({
+    url: movieApi,
+    type: "GET",
+    dataType: "json"
+  }).done((response)=>{
+    $(".option").html(`Movies matching "${search}""`)
+    response.Search.forEach(function(object){
+    console.log(object)
+    $("#movie-select").append(`<option>${object.Title}</option>`)
+ })
+
+    console.log(response.Search)
+  }).fail(() =>{
+    console.log("FAILED")
+  })
+})
