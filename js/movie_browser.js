a3810c3afa1e9664803f8bd2763e76e8d9fc1d8f
Barrett Cope <barrettcope@gmail.com>
2016-10-25 17:54:52 -0400

    Initial commit

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,16 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+
+
+const queryUrl = $("")
+
+$.ajax({
+  url: queryUrl,
+  type: "GET",
+  dataType: "json"
+}).done((response) => {
+  console.log("Success", response)
+}).fail((response) => {
+  console.log("Failed", response)
+})
