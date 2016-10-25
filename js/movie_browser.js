a90d3667a031d6ac16ceb835e171e80f3b613250
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-10-25 19:16:09 -0400

    completed step 1 and 2

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,25 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+$('#movie-select').hide()
+// $('.submit').on('click', function(){
+//   $('#movie-select').show()
+// })
+$('.submit').on('click', () => {
+  $('#movie-select').empty()
+  var url = "http://www.omdbapi.com/?s="
+  var keyword = $('#movie-search').val()
+  $.ajax({
+    url: url + keyword,
+    type: "GET",
+    dataType: "json",
+  }).done(response =>{
+response.Search.forEach(movie => {
+  $('#movie-select').append(`<option>Movies matching "${keyword}"</option>`)
+  $('#movie-select').append(`<option>${movie.Title}</option>`)
+})
+})
+$('#movie-select').show()
+
+
+
+})
