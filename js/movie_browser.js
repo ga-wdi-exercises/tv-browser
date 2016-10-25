e465d23cd3b45cdb9bb6f598a7d2f59550ffac2b
Kevin Mahoney <kjmahoney13@gmail.com>
2016-10-24 20:50:54 -0400

    populated image and title

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -7,8 +7,10 @@ submitButton.on("click", (event) => {
 const criteria = $("#movie-search").val()
 
 const apiURL = "http://www.omdbapi.com/?s=" + criteria
-// const options = [];
 
+
+$("#movie-select").empty()
+$("#movie-select").append("<option>Please select a movie</option>")
 console.log(apiURL)
 
 $.ajax({
@@ -19,16 +21,29 @@ $.ajax({
 }).done((response) =>{
   console.log("success!", response.Search[0].Title)
 
+  moviesArray = []
+
   for (i = 0; i < response.Search.length; i++) {
       console.log(response.Search[i].Title)
+       var newObject = {
+         title: response.Search[i].Title,
+         photo_url : response.Search[i].Poster
+       }
+      moviesArray.push(newObject)
       $("select").append(`<option>${response.Search[i].Title}</option>`)
   }
 
-    $("#movie-select").change(function() {
+$("#movie-select").change(function() {
       let selectedMovie = $("#movie-select").val()
       $("#movie-detail").text(`${selectedMovie}`)
-    })
 
+      for (i = 0; i < moviesArray.length; i++){
+        if (moviesArray[i].title == selectedMovie){
+          let selectedPoster = moviesArray[i].photo_url
+          $("#moviePoster").attr("src",`${selectedPoster}`);
+        }
+      }
+    })
 }).fail((response)=>{
   console.log("fail!", response)
 })
@@ -36,17 +51,3 @@ $.ajax({
 event.preventDefault();
 
 })
-
-
-
-
-
-
-// console.log("success!",response.Search[0].Title)
-
-
-
-//
-// for (i = 0; i < cars.length; i++) {
-//     text += cars[i] + "<br>";
-// }
