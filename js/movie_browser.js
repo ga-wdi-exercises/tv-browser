4485dca1f3b27c59d0e8e8f56c97467687b1b952
Domonique Griffin <griffind2009@gmail.com>
2016-10-25 18:51:22 -0400

    can search

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,21 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+$("#movie-select").hide();
+
+$("button").on("click",() => {
+  const apiUrl = "http://www.omdbapi.com/?s="
+  let title = $("#movie-search").val()
+$.ajax({
+  url: apiUrl + title,
+  type: "GET",
+  dataType: "json",
+}).done((response) => {
+  // $("body")
+  console.log(response)
+}).done((response) =>
+{ 
+  $("#movie-select").show()
+    $("#movie-detail").append(`<div>${response.title}</div>`)
+})
+
+})
