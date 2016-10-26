589083b5a2816093c6098bf1481f56e4ccbaea5c
Ian Hollands <ihollands@outlook.com>
2016-10-26 19:36:14 -0400

    3 - complete

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -15,6 +15,16 @@ $(document).ready(function(){
       for (var i=0; i<response.Search.length; i++) {
         $("#movie-select").append(`<option id=${i})>${response.Search[i].Title}</option>`)
       }
+      $("#movie-select").change(function() {
+      $("#movie-detail").empty()
+      var value = $('#movie-select').val()
+      for (var i=0; i<response.Search.length; i++) {
+        if (response.Search[i].Title === value) {
+          $("#movie-detail").append(`<h2>${response.Search[i].Title}</h2>`)
+          $("#movie-detail").append(`<div><img src="${response.Search[i].Poster}"></div>`)
+          }
+        }
+      }).change()
     }).fail((response) => {
       console.log("Ajax get request failed.");
     })
