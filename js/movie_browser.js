dfb4946c8bc1856fe142dc7695f79e9010321963
RYANBROPHY <BROPHY2321@ME.COM>
2016-10-26 08:52:50 -0400

    search

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,4 +1,14 @@
 // API Docs at:
 // http://www.omdbapi.com
+//this event hides the movie-select
+ document.getElementById("movie-select").style.visibility = "hidden";
 
-document.getElementById("movie-select").style.visibility = "hidden";
+// need to create event handler that makes movie select reppear after submit it clicke
+
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.omdbapi.com/", false);
+xhr.send();
+
+console.log(xhr.status);
+console.log(xhr.statusText);
