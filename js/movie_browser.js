3b2f791e665203ff926f7a4f7202b592e53baa1f
Nick Oki <nick.oki@live.com>
2016-10-26 09:10:21 -0400

    Hide and display movie-select

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,7 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+$(search).children().eq(1).on('click', function(e) {
+  e.preventDefault()
+  $('#movie-select').removeClass("hidden")
+})
