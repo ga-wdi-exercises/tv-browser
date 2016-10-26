c24f3d460c8940686b19c53b4d32c989d6bf6f9b
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-10-26 00:13:09 -0400

    need to append image

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -13,13 +13,40 @@ $('.submit').on('click', () => {
     type: "GET",
     dataType: "json",
   }).done(response =>{
+    $('#movie-select').append(`<option>Movies matching "${keyword}"</option>`)
 response.Search.forEach(movie => {
-  $('#movie-select').append(`<option>Movies matching "${keyword}"</option>`)
-  $('#movie-select').append(`<option>${movie.Title}</option>`)
+  console.log(movie)
+  $('#movie-select').append(`<option value="${movie.imdbID}">${movie.Title}</option>`)
+
 })
 })
 $('#movie-select').show()
 
+$('#movie-select').on('change',function(){
+  var selected = $('#movie-select').find(":selected")
+  // $('#movie-detail').append(selected.val())
+  var id = selected.val()
+  var idurl = "http://www.omdbapi.com/?i=" + id
+  $.ajax({
+    url: idurl,
+    type: "GET",
+    dataType: "json",
+  }).done((response) =>{
+    $('#movie-detail').append(response.Title);
+    var x = response.Poster
+    console.log(x)
+    $('#movie-detail').append('<img src="${response.Poster}">');
 
 
+  }).fail(() => {
+    console.log("Failed")
+  })
+})
+
+// var iurl ="http://www.omdbapi.com/?i="
+// var id = seleced.val()
+// $.ajax({
+//   finalurl: iurl + id
+//   console.log(finalurl)
+// })
 })
