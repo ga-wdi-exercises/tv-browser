024b0765b95012d21004979a8fc58c552a482c3f
RYANBROPHY <BROPHY2321@ME.COM>
2016-10-26 09:50:16 -0400

    second

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,14 +1,51 @@
+
+angular
+  .module("mb", [
+"ui.router"
+])
+.config([
+    "$stateProvider",
+    Router
+])
+.controller("IndexController", [
+  IndexController
+])
+function IndexController(){
+    this.search = function(){
+      console.log(this.searchTerm)
+      $.getJSON('http://www.omdbapi.com/?s=${this.searchTerm}', response => {
+          vm.movies = response.Search
+      })
+    }
+
+}
+
+function Router($stateProvider){
+  $stateProvider
+    .state("index", {
+      url: "/",
+      controller: 'IndexController',
+      controllerAs: 'vm',
+      templateUrl: 'js/ng-views/index.html'
+    })
+}
 // API Docs at:
 // http://www.omdbapi.com
 //this event hides the movie-select
- document.getElementById("movie-select").style.visibility = "hidden";
 
 // need to create event handler that makes movie select reppear after submit it clicke
 
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.omdbapi.com/", false);
-xhr.send();
+//bootstrap the app
 
-console.log(xhr.status);
-console.log(xhr.statusText);
+//set up a router
+
+
+//set up controller
+
+
+//some template html
+
+//ngsubmit on the form
+
+//make request to api for list of movies
