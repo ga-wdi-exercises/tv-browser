3600b952be828ef64939cafb41b2bcea91dc485d
Nick Oki <nick.oki@live.com>
2016-10-26 09:17:07 -0400

    Show/hide fix

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,7 +1,12 @@
 // API Docs at:
 // http://www.omdbapi.com
 
+$('#movie-select').hide()
+
 $(search).children().eq(1).on('click', function(e) {
   e.preventDefault()
-  $('#movie-select').removeClass("hidden")
+  $('#movie-select').show()
+
 })
+
+$('#movie-select')
