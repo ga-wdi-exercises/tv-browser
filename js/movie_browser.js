fe23ce6ecc0c374d5e84e48bf083041651bb6ef2
Nick Oki <nick.oki@live.com>
2016-10-26 10:05:24 -0400

    Populate movie-detail

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -8,20 +8,25 @@ $(search).children().eq(1).on('click', function(e) {
   $('#movie-select').show()
   let title = $('#movie-search').val()
   let apiUrl = `http://www.omdbapi.com/?s=${title}`
-  getMovies(apiUrl)
-})
-
-// $('#movie-select')
-
-var getMovies = function(apiUrl) {
   $.getJSON(apiUrl, response => {
     populateMovieSelect(response.Search)
   })
-
-}
+})
 
 var populateMovieSelect = function(arr) {
   for (i = 0; i < arr.length; i++) {
-    $('#movie-select').append(`<option>${arr[i].Title}</option>`)
+    $('#movie-select').append(`<option value="${arr[i].Title}">${arr[i].Title}</option>`)
   }
+}
+
+$('#movie-select').on('change', function() {
+  let apiUrl = `http://www.omdbapi.com/?t=${$('#movie-select').val()}`
+  $.getJSON(apiUrl, response => {
+    populateMovieDetail(response)
+  })
+})
+
+var populateMovieDetail = function(res) {
+  console.log(res);
+  $('#movie-detail').html(`<h1>${res.Title}</h1><img src="${res.Poster}" />`)
 }
