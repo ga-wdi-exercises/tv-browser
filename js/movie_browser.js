f55986184421e8f21bb72c0959e3e805432205e4
Jesse Shawl <jesse@jshawl.com>
2016-10-26 09:52:49 -0400

    add ng solution

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,52 @@
-// API Docs at: 
+angular
+.module("mb",[
+  "ui.router"
+])
+.config([
+  "$stateProvider",
+  Router
+])
+.controller("IndexController",[
+  IndexController
+])
+
+function IndexController(){
+  var vm = this
+  vm.searchTerm = "up"
+  vm.search = function(){
+    this.$apply(function(){
+
+
+    $.getJSON(`http://www.omdbapi.com/?s=${vm.searchTerm}`,function(response){
+      console.log("finished searchin")
+      vm.movies = response.Search
+      console.log(vm)
+    })
+      })
+  }
+}
+
+function Router($stateProvider){
+  $stateProvider
+  .state("index",{
+    url: '/',
+    controller:'IndexController',
+    controllerAs:'vm',
+    templateUrl: 'js/ng-views/index.html'
+  })
+}
+
+// API Docs at:
 // http://www.omdbapi.com
+
+// bootstrap the app
+
+// set up a router
+
+// set up a controller
+
+// some template html
+
+// ng-submit on the form
+
+// request list of movies from api
