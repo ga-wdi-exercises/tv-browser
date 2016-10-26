9d2978e318030e6eec124c00254fd5b3ee738773
Nick Oki <nick.oki@live.com>
2016-10-26 09:36:41 -0400

    Populate movie-select with search results

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -6,7 +6,22 @@ $('#movie-select').hide()
 $(search).children().eq(1).on('click', function(e) {
   e.preventDefault()
   $('#movie-select').show()
-
+  let title = $('#movie-search').val()
+  let apiUrl = `http://www.omdbapi.com/?s=${title}`
+  getMovies(apiUrl)
 })
 
-$('#movie-select')
+// $('#movie-select')
+
+var getMovies = function(apiUrl) {
+  $.getJSON(apiUrl, response => {
+    populateMovieSelect(response.Search)
+  })
+
+}
+
+var populateMovieSelect = function(arr) {
+  for (i = 0; i < arr.length; i++) {
+    $('#movie-select').append(`<option>${arr[i].Title}</option>`)
+  }
+}
