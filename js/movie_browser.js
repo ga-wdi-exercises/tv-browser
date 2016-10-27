1406eba058fc114125ac5d033fd9d8ddf96b2018
brown2206 <brown.angelo.jr@gmail.com>
2016-10-26 21:01:14 -0400

    added movie-select functionality

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,23 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+$("#movie-select").hide();
+
+$(".submit").on("click", () => {
+            $("#movie-select").show();
+            var url = "http://www.omdbapi.com/?s="
+            var search = $("#movie-search").val()
+            $.ajax({
+                url: url + search,
+                type: "get",
+                dataType: "json"
+            }).done(response => {
+              console.log(response.Search[0].Title)
+
+
+                    for (var i = 0; i < response.Search.length; i++) {
+                        console.log(response.Search[i].Title)
+                        results = response.Search[i].Title
+                        $("#movie-select").append("<option>" + results + "</option>")
+                    }
+                })
+})
