c39aac748f1c7535f8238cd7dbc1e0dc4eb18d2e
YOUR FULL NAMEAndres <a_espinosa@live.com>
2016-10-26 23:18:07 -0400

    initial commit

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,16 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+$('submit').on('click', () => {
+  const url = "http://img.omdbapi.com/?i=tt2294629&apikey=286bb3d8";
+
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+
+}).done((response) => {
+  console.log(response);
+}).fail(() => {
+  console.log("Sorry, we had issues retrieving movie data!")
+})
