1679b750f8d377a49f6396aecd7080a17723129a
Mark Sweet <marks828@gmail.com>
2016-10-27 08:49:35 -0400

    movie homework

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,25 @@
-// API Docs at: 
-// http://www.omdbapi.com
+// API Docs at:
+//
+
+ $("#movie-select").hide()
+
+ $('.submit').on("click", (e) => {
+   e.preventDefault()
+   let movieSelect =  $('#movie-select')
+   let sUrl = "http://www.omdbapi.com/\?s="
+   let url = sUrl + ($("#movie-search").val())
+   $.ajax({
+     url: url,
+     type: "GET",
+     dataType: "json"
+   }).done((response) => {
+     console.log("success", response);
+     movieSelect.show()
+     $('option').text('Movies Matching' + $('#movie-search').val())
+     for (let i=0; i<response.Search.length; i++){
+       $('select').append("<option>" + response.Search[i].Title + "</option")
+     }
+   }).fail(() => {
+     console.log("fail");
+   })
+ })
