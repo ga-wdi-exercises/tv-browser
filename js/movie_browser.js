d9aced6b0fe42add3a58457b6bf17fd775e9aa4e
Liza Floyd <lizafloyd@gmail.com>
2016-10-26 21:04:44 -0400

    adds title to page

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,22 +1,28 @@
 // API Docs at:
 // http://www.omdbapi.com
 
-  $(".submit").on("click", function(e){
-    e.preventDefault()
-    let search = $('#movie-search').val()
-    $.ajax({
-      url: `http://www.omdbapi.com/?s=${search}`,
-      type: "get",
-      dataType: "json"
-    }).done(function(response){
-      let movies = response.Search
-      movies.forEach(function(movie){
-        console.log(`${movie.Title}`);
-        $('#movie-select').show();
-        $('#movie-select').append( $("<option>").val("value").html(`${movie.Title}`));
-      })
+$(".submit").on("click", function(e){
+  e.preventDefault()
+  let search = $('#movie-search').val()
+  $.ajax({
+    url: `http://www.omdbapi.com/?s=${search}`,
+    type: "get",
+    dataType: "json"
+  }).done(function(response){
+    let movies = response.Search
+    movies.forEach(function(movie){
+      console.log(`${movie.Poster}`);
+      $('#movie-select').show();
+      $('#movie-select').append( $("<option>").val(`${movie.Title}`).html(`${movie.Title}`));
+    })
     })
   })
+
   $("#movie-select").on("change", function(){
-    console.log("this is where i left off liza!");
-  })
+    let search = $("#movie-select").val()
+    $("body").append(`<div>${search}</div>`)
+    //my instinct here is to do a second ajax call...that doesn't seem right?
+    $("body").append(`<img src="${movie.Poster}"> </img>`)
+
+    console.log(search);
+})
