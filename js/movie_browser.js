0c61739ddc09709d960c915f0b320d322e19b000
Chris Jaffier <chrisd@Chriss-MacBook-Pro.local>
2016-10-27 08:46:35 -0400

    added ajx function

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -8,3 +8,13 @@ var url = "http://www.omdbapi.com"
     $("#movie-search").on("submit").val()
     var word = $("#movie-search")
       console.log(word)
+/*
+    }).done(() => {
+ console.log("Ajax request success!")
+}).fail(() => {
+ console.log("Ajax request fails!")
+}).always(() => {
+ console.log("This always happens regardless of successful ajax request or not.")
+})
+})
+*/
