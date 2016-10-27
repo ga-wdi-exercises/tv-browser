4e139c1e984a98e130c713baa65dce08eb7342d5
Alexander <ajspottswood@gmail.com>
2016-10-26 20:40:27 -0400

    search results populated

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,3 +1,22 @@
 // API Docs at:
 // http://www.omdbapi.com
-$("#movie-select").hide();
+$("#movie-select").hide()
+$(".submit").on("click", function(event){
+  event.preventDefault()
+  let search = $("#movie-search").val()
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: `http://www.omdbapi.com/?s=${search}`
+  }).done((response) => {
+    console.log(response)
+    let movies = response.Search
+    $('select').html('')
+    movies.forEach((movie) => {
+      $('select').append(`<option value=${movie.imdbID}>${movie.Title}</option>`)
+    })
+  }).fail((response) => {
+    console.log(response)
+    })
+    $('select').show()
+  });
