526d3c80092bd645b6d3324b4ce565da26583750
Angel Valant <angelgithub@gmail.com>
2016-11-24 10:42:04 -0500

    initial commit

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,26 @@
-// API Docs at: 
-// http://www.omdbapi.com
+$("#movie-select").hide()
+
+$("#submit").on("click", function(){
+  event.preventDefault()
+  let url = "http://www.omdbapi.com/?s="
+  let keyword = $("#movie-search").val()
+
+  $.ajax({
+    url: url + keyword,
+    type: "get",
+    dataType: "json"
+
+  }).done(function(response){
+    // console.log(response)
+    response.Search.forEach(function(movies, i){
+      // console.log(movies)
+      $("#movie-select").append(`<option value='${i}'>${movies.Title}</div>`)
+      $("#movie-select").show()
+      $("#movie-select").change("click", function(){
+        event.preventDefault()
+        // console.log(movies.Title + i)
+        $("#movie-detail").append(`<h1>${movies.Title}</h1><img src="${movies.Poster}">`)
+      })
+    })
+  })
+})
