f140c0cca530bd2b109532168ebb8d69e77a5bfc
Drew Swinburne <drewswinburne@gmail.com>
2016-11-26 18:08:39 -0500

    non-angular solution

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,50 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+angular.module("movieBrowser", [])
+    .config(["$stateProvider", RouterFunction])
+    .controller('MovieIndexController', [MovieIndexControllerFunction])
+
+
+function MovieIndexControllerFunction(){
+
+};
+
+$("#movie-select").hide();
+
+  // console.log("jquery working")
+  $("#search").on("submit", function(event){
+    event.preventDefault()
+    console.log("hellooo")
+    $("#movie-select").show();
+
+
+    let title = $('#movie-search').val()
+    let url= `http://www.omdbapi.com/?s=${title}&y=&plot=short&r=json`
+    $.ajax({
+      url,
+      type: "get", // making a get request
+      dataType: "json"
+    }).done((response) => {
+      $("#movie-select").append(`<option>movies matching "${title}"</option>`)
+      response.Search.forEach(function(movie){
+      console.log(movie.Title)
+      let option = $(`<option value="${movie.Title}">${movie.Title}</option>`)
+      $("#movie-select").append(option)})
+    }).fail(() => {
+      console.log("ajx fails...")
+    }).always(() => {
+      console.log("this happens always") //these are "promises"
+    })
+
+  });
+
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("movieIndex", {
+      url: "/",
+      controller: "MovieIndexController",
+      controllerAs: "vm"
+    })
+  }
