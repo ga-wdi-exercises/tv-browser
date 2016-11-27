18490ef238b7334fe81cdcbcec13e07a088584e6
Rob Romaniw <robertromaniw@gmail.com>
2016-11-27 17:02:57 -0500

    Linked API URL

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,34 @@
-// API Docs at: 
-// http://www.omdbapi.com
+/*jshint esversion: 6 */
+
+$(document).ready(function() {
+    //console.log( "ready!" );
+
+    $('#search').on('submit', function(pd) {
+        pd.preventDefault();
+        // console.log(this);
+        var keyword = $('#movie-search').val();
+        findAll(keyword);
+    });
+
+
+    function findAll(keyword) {
+        var url = 'http://www.omdbapi.com/?s=' + keyword;
+        $.ajax({
+            url: url,
+            type: "get",
+            dataType: "json"
+                // $.ajax takes an object as an argument with at least three key-value pairs...
+                // (1) The URL endpoint for the JSON object.
+                // (2) Type of HTTP request.
+                // (3) Datatype. Usually JSON.
+        }).done(() => {
+            console.log("Ajax request success!");
+        }).fail(() => {
+            console.log("Ajax request fails!");
+        }).always(() => {
+            console.log("This always happens regardless of successful ajax request or not.");
+        });
+    }
+
+
+});
