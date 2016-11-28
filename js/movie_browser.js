d9ada076745cde50786353ab29c86829c045e883
Sam Catherman <Sam.Catherman@gmail.com>
2016-11-28 07:22:02 -0500

    initial commit

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,30 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+$("#movie-select").hide();
+
+$("#submit").on("click", function(){
+  event.preventDefault();
+  var url="http://www.omdbapi.com/?s="
+  var searchTerm = $("#movie-search").val()
+
+  $.ajax({
+    url: url + searchTerm,
+    type: "get",
+    dataType: "json"
+  }).done(function(response){
+    response.Search.forEach(function(movie){
+      $("#movie-select").append('<option>' + movie.Title + '</option>')
+    })
+
+    $("#movie-select").on("change", () => {
+      let movieChoice = $("#movie-select").val()
+
+        for (i=0; i< response.Search.length; i++) {
+          if (response.Search[i]["Title"] == movieChoice) {
+            $("#movie-detail").empty().append('<h2>' + response.Search[i]["Title"]+'</h2><img src="'response.Search[i]["Poster"]+'" />')
+          }
+        }
+    })
+  })
+})
