571adfc83f538f8e7aa372754fae5273c761c170
Hannah Schuster <hschuster9@gmail.com>
2017-03-06 18:43:56 -0500

    number 1

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,17 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+$("#movie-select").hide();
+
+$("#submit").on("click", () =>{
+ let url = "http://www.omdbapi.com/";
+
+ $.ajax({
+   url: url,
+   type: "get",
+   dataType: "json"
+ }).done((response) => {
+   $("#movie-select").show();
+   $("#movie-search").val();
+ })
+});
