6e679bceaab1f89b4c886f90f9a573844066cb5c
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-03-07 17:51:07 -0500

    initial commit

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,30 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+$(document).ready(() => {
+
+  $("#movie-select").hide().on('change', function(){
+    show(this.value);
+  });
+
+  $(".submit").on("click", () => {
+    event.preventDefault();
+
+    let search = $("#movie-search").val();
+    let url = `http://www.omdbapi.com/?s=${search}`
+
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: url
+    }).done((response) => {
+      let movies = response.Search
+      $("select").html('')
+      movies.forEach((movie) => {
+        $("#movie-select").append(`<option value="${movie.imdbID}">${movie.Title}</option>`)
+      })
+    }).fail((response) => {
+      console.log("Ajax failed");
+    })
+    $("#movie-select").show().html()
+  })
+})
