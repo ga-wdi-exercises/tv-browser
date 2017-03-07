dae6fdd8956e028b2cf79bf0d53d3ceb7d1e4f0b
Habtamu Abere <habbuya@gmail.com>
2017-03-07 14:24:57 -0700

    hide and show div

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,9 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+function myFunction(){
+
+  $("#movie_select").css("display", "inline-block");
+}
