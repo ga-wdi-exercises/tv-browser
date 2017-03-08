1b924cb849c01f3954e184c95874cf8f9541fdb3
Andy Flickinger <atflick@gmail.com>
2017-03-08 08:49:08 -0500

    hw done

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,40 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+
+
+
+$("#search").submit((event) => {
+  event.preventDefault();
+  let searchVal = $("#movie-search").val();
+  let url = "http://www.omdbapi.com/?s=" + searchVal;
+
+
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+  }).done((response) => {
+    $("#movie-select").empty();
+    $("#movie-select").append($("<option>Movies matching keyword...</option>"));
+    responses = response.Search;
+    $.each(responses, (index, value) => {
+      $("#movie-select").append($("<option></option>")
+        .attr("value", index).text(value.Title))
+    })
+    console.log(responses);
+    $("#movie-select").fadeIn("slow");
+  }).fail(() => {
+    console.log("API FAILED");
+  })
+})
+
+$("#movie-select").change(() => {
+
+  let val = $("#movie-select option:selected").val();
+  let title = `<h2>${responses[val].Title}</h2>`;
+  let year = `<h4>${responses[val].Year}</h4>`;
+  let posterUrl = `<img src="${responses[val].Poster}">`;
+
+  $("#movie-detail").html(title + year + posterUrl);
+})
