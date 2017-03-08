6cd1f231cd2cd541d4f3feb3cb78977c6f970fad
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-03-08 17:19:16 -0500

    search function is working

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -2,15 +2,38 @@
 $("input[type='submit']").on("click", function(keyword){
     event.preventDefault();
     $("#movie-select").css("display", "inline-block")
+    var $search = $('#movie-search');
+    var keyword = $search.val();
+    $search.val('');
 
   var url = 'http://www.omdbapi.com/?s='+escape(keyword);
   $.ajax({
     url: url,
     type: "get",
     dataType: "json"
-  }).done(() => {
-    console.log("Ajax request sucessful")
+  }).done((response) => {
+    movieDone(keyword, response);
   }).fail(() => {
-    console.log("failed")
+    console.log("Ajax failed")
   })
-})
+});
+
+function movieDone(searchKeyword, searchData){
+  var display = '<option value="">Movies matching "'+ searchKeyword +'"...</option>';
+
+  for (var i=0; i < searchData.Search.length; i++) {
+    var movie = searchData.Search[i];
+    display += ['<option value="', movie.imdbID, '">', movie.Title, '</option>'].join('');
+  }
+
+  $('#movie-select').show().html(display);
+}
+
+$('#search').on('submit', function(evt) {
+  evt.preventDefault();
+  var $search = $('#movie-search');
+  var keyword = $search.val();
+  $search.val('');
+
+  search(keyword);
+});
