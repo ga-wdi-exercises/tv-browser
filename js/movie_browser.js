23f57522ff60203b4aa75339685c41a5be651135
Eva Lai <evalai.dc@gmail.com>
2017-03-07 22:23:32 -0500

    added functionality of selecting movie and image appears.

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,33 +1,36 @@
 // API Docs at:
 // http://www.omdbapi.com
 $(document).ready(() => {
-  $("#movie-select").hide();
+    $("#movie-select").hide();
 
-  $(".submit").on("click", () => {
-    event.preventDefault()
-    let search = $("#movie-search").val()
-    let url = `http://www.omdbapi.com/?s=${search}`
-    $("#movie-select").show();
-    // removes all options from the select movie pull down bar
-    $("#movie-select").empty();
+    $(".submit").on("click", () => {
+        event.preventDefault()
+        let search = $("#movie-search").val()
+        let url = `http://www.omdbapi.com/?s=${search}`
+        $("#movie-select").show();
+        // removes all options from the select movie pull down bar
+        $("#movie-select").empty();
 
-    $.ajax({
-      url: url,
-      type: "get",
-      dataType: "json"
-    }).done((response) => {
-      // shows what was searched
-      $("#movie-select").append(`<option> Movies matching "${search}"...</option>`)
-      // runs through every result from the api search
-      for (var i=0; i < response.Search.length; i++) {
-        $("#movie-select").append(`<option value=${response.Search[i]}>${response.Search[i].Title}</option>`)
-      }
-      // $("#movie-select").append(`<option value="${}"></option>`)
-    }).fail(() => {
-      console.log("Search request fails!")
+        $.ajax({
+            url: url,
+            type: "get",
+            dataType: "json"
+        }).done((response) => {
+          // shows what was searched
+          $("#movie-select").append(`<option> Movies matching "${search}"...</option>`)
+          // runs through every result from the api search
+          for (var i=0; i < response.Search.length; i++) {
+              $("#movie-select").append(`<option data-value=${response.Search[i].Poster}>${response.Search[i].Title}</option>`)
+          }
+          $("#movie-select").change(function(){
+            let photoUrl = $(this).find(":selected").data("value")
+            $("img").attr("src", `${photoUrl}`)
+          })
+
+        }).fail(() => {
+          console.log("Search request failed!")
+        })
     })
-
-  })
 
 
 })
