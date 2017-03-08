d8407c4a2bc1861fb5afc1ec6b1e8d5c50f72342
Elena Santana <fanelenan7@gmail.com>
2017-03-08 16:46:46 -0500

    trying out this API thing

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,15 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+$("button").click( () => {
+  let url = "http://www.omdbapi.com/?s='+escape(keyword)"
+
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+  }).done( (response) => {
+    $("body").append($(`<div>Current wind gust is up to ${response.current_observation.wind_gust_mph} mph.</div>`))
+
+  })
+})
