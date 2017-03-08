47c08170d3ea4073a8ca5712543344a5b6e3e823
Taylor Scott <TaylorScott@Taylors-MacBook-Pro.local>
2017-03-08 14:59:37 -0500

    homeworkish

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,40 @@
-// API Docs at: 
-// http://www.omdbapi.com
+angular
+  .module("mb",[
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    "$locationProvider",
+    RouterFunc
+  ])
+  .controller("IndexController", [
+    "Movie",
+    IndexController
+  ])
+  .factory("Movie", [
+    "$resource",
+    MovieFactoryCallback
+  ])
+
+function IndexController(Movie){
+  this.searchTerm = "up"
+  this.search = function(){
+    Movie.get({searchTerm:this.searchTerm}).$promise.then( (res) => this.movies = res.Search );
+  }
+}
+
+function MovieFactoryCallback($resource){
+  return $resource("http://www.omdbapi.com/?s=:searchTerm");
+}
+
+function RouterFunc($stateProvider, $locationProvider){
+  $locationProvider.html5Mode(true);
+  $stateProvider
+  .state("index", {
+    url: '/',
+    templateUrl: 'js/ng-views/index.html'
+    controller: 'IndexController',
+    controllerAs: 'vm',
+  })
+}
