81f600df10a8cf4084a30c4e99daa44f4b1bebe1
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-03-08 18:50:27 -0500

    starting from scratch-drop down works

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,48 +1,54 @@
 
-$("input[type='submit']").on("click", function(keyword){
-    event.preventDefault();
-    $("#movie-select").css("display", "inline-block")
-    var $search = $('#movie-search');
-    var keyword = $search.val();
-    $search.val('');
-
-  var url = 'http://www.omdbapi.com/?s='+escape(keyword);
+$("input[type='submit']").on("click", function(){
+  event.preventDefault();
+  var keyword = $("#movie-search").val()
+  var url = `http://www.omdbapi.com/?s=${keyword}`
+  console.log(url)
   $.ajax({
     url: url,
     type: "get",
     dataType: "json"
   }).done((response) => {
-    movieDone(keyword, response);
+    console.log(response)
+    for (var i =0; i < response.Search.length; i++){
+      var movie = response.Search[i]
+      $("#movie-select").append(`<option value = "${movie.Title}">${movie.Title}</option>`)
+    }
+    $("#movie-select").css("display", "inline-block")
+    movieOption = $("#movie-select").find(":selected").val()
+    console.log(movieOption)
   })
 
-})
+  
 
-function movieDone(searchKeyword, searchData){
-  var display = '<option value="">Movies matching "'+ searchKeyword +'"...</option>';
 
-  for (var i=0; i < searchData.Search.length; i++) {
-    var movie = searchData.Search[i];
-    display += ['<option value="', movie.imdbID, '">', movie.Title, '</option>'].join('');
-  }
 
-  $('#movie-select').show().html(display);
-
-    var detail = '<h2>' + movie.Title + '</h2>';
-    detail += '<img src="'+ movie.Poster +'" alt="'+ movie.Title +'">';
-    $('#movie-detail').html(detail);
-  };
-
-$('#search').on('submit', function(evt) {
-  evt.preventDefault();
-  var $search = $('#movie-search');
-  var keyword = $search.val();
-  $search.val('');
-
-  search(keyword);
+// function movieDone(searchKeyword, searchData){
+  // var display = '<option value="">Movies matching "'+ searchKeyword +'"...</option>';
+  //
+  // for (var i=0; i < searchData.Search.length; i++) {
+  //   var movie = searchData.Search[i];
+  //   display += ['<option value="', movie.imdbID, '">', movie.Title, '</option>'].join('');
+  // }
+  //
+  // $('#movie-select').show().html(display);
+  //
+  //   var detail = '<h2>' + movie.Title + '</h2>';
+  //   detail += '<img src="'+ movie.Poster +'" alt="'+ movie.Title +'">';
+  //   $('#movie-detail').html(detail);
 });
-
-function show(imdbId) {
-  if (!imdbId) return;
-
-  var url = 'http://www.omdbapi.com/?i='+imdbId;
-}
+//
+// $('#search').on('submit', function(evt) {
+//   evt.preventDefault();
+//   var $search = $('#movie-search');
+//   var keyword = $search.val();
+//   $search.val('');
+//
+//   search(keyword);
+// });
+//
+// function show(imdbId) {
+//   if (!imdbId) return;
+//
+//   var url = 'http://www.omdbapi.com/?i='+imdbId;
+// }
