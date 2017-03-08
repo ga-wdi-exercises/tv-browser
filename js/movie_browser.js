3e3a7c9d755ebb2f1530415ae85276604669d070
Hannah Schuster <hschuster9@gmail.com>
2017-03-08 14:28:47 -0500

    completed

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,17 +1,38 @@
 // API Docs at:
 // http://www.omdbapi.com
 
-$("#movie-select").hide();
+$(document).ready(()=> {
+  $("#movie-select").hide();
+})
 
 $("#submit").on("click", () =>{
- let url = "http://www.omdbapi.com/";
-
+  event.preventDefault();
+  $("#movie-select").show();
+ let keyword = $("#movie-search").val();
+ let url = `http://www.omdbapi.com/?s=${keyword}`
+ $('#movie-select').empty();
  $.ajax({
    url: url,
-   type: "get",
+   type: "GET",
    dataType: "json"
  }).done((response) => {
-   $("#movie-select").show();
-   $("#movie-search").val();
- })
-});
+   $("#movie-select").append(`<option> "Movies matching "${keyword}"</option>`)
+
+   for(var i = 0; i < response.Search.length; i++){
+
+     $("#movie-select").append(`<option data-value=${response.Search[i].Poster}> ${response.Search[i].Title} </option>`)
+   }
+
+  $("#movie-select").change(function(){
+    let title = $(this).find(":selected").html();
+    $('#movie-detail > h2').html(title)
+
+    let photo = $(this).find(":selected").data("value")
+    $('img').attr("src", `${photo}`)
+  })
+
+
+}).fail(() => {
+  console.log("Failed")
+})
+})
