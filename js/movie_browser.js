249bbd4db69b6665d6eea881cd8c972c55533731
Lisa Anne Burr <burr_lisa@bah.com>
2017-03-08 15:02:26 -0500

    initial commit

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,39 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+angular
+  .module("movieBrowser",  [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$locationProvider",
+    "$stateProvider",
+    RouterFunction
+  ])
+  .factory( "MovieBrowserFactory", [
+    "$resource",
+    MovieBrowserFactoryFunction
+  ])
+  .controller("MovieBrowserIndexController", [
+    "MovieBrowserFactory",
+    MovieBrowserIndexControllerFunction
+  ])
+
+function RouterFunction($locationProvider, $stateProvider){
+  $locationProvider.html5Mode(true);
+  $stateProvider
+  .state("movieIndex",  {
+    url: "/",
+    templateUrl: "js/ng-views/index.html",
+    controller: "MovieBrowserIndexController",
+    controllerAs: "vm"
+  })
+}
+
+function MovieBrowserFactoryFunction($resource){
+  return $resource( "http://www.omdbapi.com/?s=:searchTerm" );
+}
+
+function MovieBrowserIndexControllerFunction( MovieBrowserFactory){
+  this.movies = MovieBrowserFactory.query();
+}
