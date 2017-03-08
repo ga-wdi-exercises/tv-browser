fff5e735ef8818cccdb7cbdff36dd170b8126366
Collin OConnell <ococncol@gmail.com>
2017-03-07 23:40:33 -0500

    got all functions working able to search and view  all movies

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,50 +1,54 @@
-// API Docs at:
-//http://www.omdbapi.com/?
-
-//http://img.omdbapi.com/?apikey=2efbebb6&
-
-// http://www.omdbapi.com
-
-// $(document).ready(function(){
-//   $('#movie-select').hide(),on('change', ()=>{});
-//
-//
-//
-//
-//
-// });
-
-
-
 $(document).ready(function() {
+    // API Docs at:
+    //http://www.omdbapi.com/?
+    function search(movieName) {
+        var url = `http://www.omdbapi.com/?s=${movieName}`
 
-    $('#movie-select').hide().on('change', () => {});
-
-    $('#btn').on('click', () => {
-        event.preventDefault()
-
-        var search = $('#movie-search').val();
-        var url = `http://www.omdbapi.com/?s=${search}`
-
-        $.ajax({
-            type: 'GET',
-            dataType: 'json',
-            url: url
-        }).done((response) => {
-            console.log(response);
-            // console.log(response);
-            var movies = response.Search
-            $('select').html('')
-            movies.forEach((movie) => {
-                selected = $('#movie-select').append(`<option value="${movie.imdbID}">${movie.Title}</option>`)
-                $('#movie-detail').text(selected.text())
+        $.getJSON(url)
+            .done(function(imdbResponse) {
+                imdbDone(movieName, imdbResponse)
             })
-        }).fail((response) => {
-            console.log("Ajax failed");
+            .fail(function(imdbResponse, textStatus, errorMessage) {
+                let message = 'Sorry we could not find data for "' + movieName + '""'
+                if (errorMessage) {
+                    message += "(" + errorMessage + ")"
+                }
+                message += ". Please try again"
+                $('#movie-detail').html("<h2 class='fail'>" + message + "</h2>")
+            })
+    }
+
+    function imdbDone(searchMovieName, imdbSearchData) {
+        let display = '<option value="">Movies matching"' + searchMovieName + '"...</option>'
+
+        for (i = 0; i < imdbSearchData.Search.length; i++) {
+            let movie = imdbSearchData.Search[i]
+            display += ['<option value="', movie.imdbID, '">', movie.Title, '</option>'].join('')
+        }
+        $('#movie-select').show().html(display)
+    }
+
+    function show(imdbId) {
+        if (!imdbId) return
+        let url = 'http://www.omdbapi.com/?i=' + imdbId
+
+        $.getJSON(url).then(function(imdbMovieData) {
+            let detail = '<h2>' + imdbMovieData.Title + '</h2>'
+            detail += '<img src="' + imdbMovieData.Poster + '" alt="' + imdbMovieData.Title + '">'
+            $('#movie-detail').html(detail)
         })
-        $('#movie-select').show().html()
+    }
 
+    $('#search').on('submit', function(evt) {
+        evt.preventDefault()
+        let $search = $('#movie-search')
+        let movieName = $search.val()
+        $search.val('')
 
+        search(movieName)
+    })
 
+    $('#movie-select').hide().on('change', function() {
+        show(this.value);
     });
 });
