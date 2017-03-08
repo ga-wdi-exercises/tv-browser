94f6096ba8aea46fd174c3a76879a37148c4440d
Collin OConnell <ococncol@gmail.com>
2017-03-07 21:39:36 -0500

    got menu to display now trying to get  text to display when picking movie

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,50 @@
-// API Docs at: 
+// API Docs at:
+//http://www.omdbapi.com/?
+
+//http://img.omdbapi.com/?apikey=2efbebb6&
+
 // http://www.omdbapi.com
+
+// $(document).ready(function(){
+//   $('#movie-select').hide(),on('change', ()=>{});
+//
+//
+//
+//
+//
+// });
+
+
+
+$(document).ready(function() {
+
+    $('#movie-select').hide().on('change', () => {});
+
+    $('#btn').on('click', () => {
+        event.preventDefault()
+
+        var search = $('#movie-search').val();
+        var url = `http://www.omdbapi.com/?s=${search}`
+
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: url
+        }).done((response) => {
+            console.log(response);
+            // console.log(response);
+            var movies = response.Search
+            $('select').html('')
+            movies.forEach((movie) => {
+                selected = $('#movie-select').append(`<option value="${movie.imdbID}">${movie.Title}</option>`)
+                $('#movie-detail').text(selected.text())
+            })
+        }).fail((response) => {
+            console.log("Ajax failed");
+        })
+        $('#movie-select').show().html()
+
+
+
+    });
+});
