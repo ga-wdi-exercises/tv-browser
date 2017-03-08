ea45422f74a60e91f864308bb04d4f39e296401c
Bedig Galladian <bgalladian@gmail.com>
2017-03-08 13:44:10 -0500

    got movies to show up in the drop down menu for select

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,8 +1,24 @@
-// API Docs at:
-// http://www.omdbapi.com
+$("#search").submit((event) => {
+  event.preventDefault();
+  let searchVal = $("#movie-search").val();
+  let url = "http://www.omdbapi.com/?s=" + searchVal;
 
-function movieSelectView() {
 
-$('#movie-select').css("display", "inline-block");
-
-}
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+  }).done((response) => {
+    $("#movie-select").empty();
+    $("#movie-select").append($("<option>Movies matching keyword...</option>"));
+    responses = response.Search;
+    $.each(responses, (index, value) => {
+      $("#movie-select").append($("<option></option>")
+        .attr("value", index).text(value.Title))
+    })
+    console.log(responses);
+    $("#movie-select").fadeIn("slow");
+  }).fail(() => {
+    console.log("API FAILED");
+  })
+})
