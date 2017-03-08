5009a56cbaffe3f69322ae76f49387cc3dd9c832
Nairul <awhitehurst4@gmail.com>
2017-03-08 15:35:18 -0500

    initial commit

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,16 @@
-// API Docs at: 
-// http://www.omdbapi.com
+
+$(".submit").on("click", () => {
+	let url = 'http://www.omdbapi.com/?s='+escape(keyword);
+
+	$.ajax({
+		url: url,
+		type: "get",
+		dataType: "json"
+	}).done((response) => {
+		var plot = console.log(response.plot)
+	}).fail(() => {
+		console.log("AJAX not succesful")
+	}).always(() => {
+		console.log("AJAX always will execute")
+	})
+})
