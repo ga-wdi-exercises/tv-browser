a9ffb71051e0c9a25ad61040df87994d9135310d
Habtamu Abere <habbuya@gmail.com>
2017-03-07 23:21:21 -0700

    this is the least fun thing I did that involves movies

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -33,26 +33,41 @@ function runFunction(){
     console.log(response);
      movies = response.Search
     $("select").html('')
+      $('#movie_select').append(`<option> Movies matching "${search}" </option>`)
     movies.forEach((movie)=> {
 
-      $('#movie_select').append(` ${search} <option>${movie.Title}</option>`);
+      $('#movie_select').append(` <option value="${movie}">${movie.Title}</option>`)
+                        .append(` <option value="${movie.imdbID}">${movie.Title}</option>`);
+    //  id = movie.id
       // $('#movie_detail').append(`<option>${movie.Title}</option>`);
 
     });
 
-    $( "#movie_select" ).change(function() {
-    $('#movie_detail').append(this.value);
-
     // $('#movie_detail').append(`${movies[1].title}`);
-  });
+  // });
 
     console.log("Ajax request success!")
     console.log(`this is the movie ${response.Search[0]}`);
   }).fail(() => {
     console.log("Ajax request fails!")
-    $('#movie_detail').append()
+
 
   }).always(() => {
     console.log("This always happens regardless of successful ajax request or not.")
   })
 }
+$("#movie_select" ).hide().change(function() {
+
+
+$.ajax({
+  url: `http://www.omdbapi.com/?i=${this.value}`,
+  type: 'get',
+  dataType: 'json',
+}).done(response =>{
+console.log(response);
+$('#movie_detail').empty()
+                  .append(`<h1>${response.Title}</h1>`)
+                  .append(`<h2>${response.Actors}</h2>`)
+                  .append(`<img src="${response.Poster}" alt="image">`);
+})
+})
