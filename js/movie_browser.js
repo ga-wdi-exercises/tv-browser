c5eaeacaedf607539117568ca43fa50ee51eaa31
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-03-08 14:33:51 -0500

    ajax is successful

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,16 @@
-// API Docs at: 
-// http://www.omdbapi.com
+
+$("input[type='submit']").on("click", function(keyword){
+    event.preventDefault();
+    $("#movie-select").css("display", "inline-block")
+
+  var url = 'http://www.omdbapi.com/?s='+escape(keyword);
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+  }).done(() => {
+    console.log("Ajax request sucessful")
+  }).fail(() => {
+    console.log("failed")
+  })
+})
