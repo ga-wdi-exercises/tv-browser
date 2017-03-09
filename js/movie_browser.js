5552de0703f5d86e41613af3bd29d21c131f5de5
Elena Santana <fanelenan7@gmail.com>
2017-03-08 19:04:11 -0500

    movie list showing up

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,15 +1,31 @@
 // API Docs at:
 // http://www.omdbapi.com
+$(document).ready(() => {
+  $("#search").on('submit', () => {
+    event.preventDefault()
+    // console.log("clicked");
+    let searchQuery = $("#movie-search").val()
+    let url = `http://www.omdbapi.com/?s=${searchQuery}`;
 
-$("button").click( () => {
-  let url = "http://www.omdbapi.com/?s='+escape(keyword)"
-
-  $.ajax({
-    url: url,
-    type: "get",
-    dataType: "json"
-  }).done( (response) => {
-    $("body").append($(`<div>Current wind gust is up to ${response.current_observation.wind_gust_mph} mph.</div>`))
-
+    $.ajax({
+      url: url,
+      type: "get",
+      dataType: "json"
+    })
+    .done((response) => {
+      // console.log(response);
+      $(".default").html(`<option class ="default" value="">Movies matching "${searchQuery}"...</option>`)
+      // $("#movie-select").append(`<option value="">Movies matching ${searchQuery}...</option>`)
+      $("#movie-select").show()
+      response.Search.forEach((movie) => {
+        $("#movie-select").append(`<option value="${movie.Title}">${movie.Title}</option>`)
+      })
+      // $(".browse").append($(`<div class='response'>Movies that match ${searchQuery}.</div>`))
+    })
+    .fail(() => {
+      console.log("fail");
+      $(".default").html(`<option class ="default" value="">Sorry, no movies found.</option>`)
+()
+    })
   })
 })
