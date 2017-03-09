411e667e958ee1e27509da01aba53b7e3647c7b2
Amilcar Sylvester <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-03-09 01:02:55 -0500

    moviebrowser

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,27 +1,24 @@
 // API Docs at:
 // http://www.omdbapi.com
 
-$('#movie-select').hide().on('change', function() {
-  show(this.value);
-});
+$("#search").on("submit", function(){
+  event.preventDefault()
+  var title = $('#movie-search').val()
 
-function movieBrowserFunction() {
-
-  var search = $("#movie-search").val()
-  var url = `http://www.omdbapi.com/?s=${search}`
   $.ajax({
-    url: url,
+    url: `http://www.omdbapi.com/?s=${title}`,
     type: "get",
     dataType: "json"
     // $.ajax takes an object as an argument with at least three key-value pairs...
     // (1) The URL endpoint for the JSON object.
     // (2) Type of HTTP request.
     // (3) Datatype. Usually JSON.
-  }).done(() => {
-    console.log("Ajax success")
-  }).fail(() => {
-    console.log("Ajax fails")
-  }).always(() => {
-    console.log("This always happens regardless.")
-  })
-}
+  }).done((response) => {
+    console.log(response);
+    $(#movie-select).append(`<option> Movies that match your search "${keyword}"... </option>`)
+
+  var movieSearch = response.Search
+
+  for (var i = 0 ; i < response.Search.length; i++) {
+    $("#movie-select").append(`<option data-value=${response.Search[i].Poster}>${response.Search[i].Title} </option>`);
+  }
