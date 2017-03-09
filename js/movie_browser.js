1b4dcac41483f6844e8c0bf5578fe5ee53f01cec
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-03-08 21:04:59 -0500

    crazy train guesses

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,19 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+$(document).ready(()=>{
+  let input = 0
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: "/movie_browser"
+  }).done((response) =>  {
+    input = response
+  }).fail((response) => {
+    console.log("Ajax get request failed.");
+  })
+  for (i=0;i<input.length;i++) {
+    $(".browse").append("<div><p>" + input[i].title + " is the title of this movie </p> <img src='"+input[i].img_url+"'</div>")
+  }
+
+})
