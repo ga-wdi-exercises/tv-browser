de1e4da864f649641b70941348b615bc73caba2f
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-08 21:21:49 -0500

    doesn't work

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -4,6 +4,12 @@
 $(document).ready(()=> {
 
   $("#movie-select").hide();
-  
 
-});
+  $("#submit").on("click", ()=> {
+    event.preventDefault()
+
+    let search = $("#movie-search").val
+    let url = "http://www.omdbapi.com/?="
+  })
+
+})
