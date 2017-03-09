1ddf5eadd412b562689ed8252b90e048c165127b
585873 <gill_gurpreet2@bah.com>
2017-03-09 06:40:04 -0500

    Add files via upload

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,34 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+$(document).ready(() => {
+   $("#movie-select").hide();
+
+   $(".find").on("click", () => {
+     $("#movie-select").show();
+      event.preventDefault()
+
+      let search = $("#movie-search").val()
+      let url = `http://www.omdbapi.com/?s=${search}`
+
+      $("#movie-select").empty();
+
+      $.ajax({
+         url: url,
+         type: "get",
+         dataType: "json"
+       }).done((response) => {
+         console.log("After search movies")
+      $("#movie-select").append(`<option> Movies that matches with the keyword: "${search}"...</option>`)
+
+         //had to look at the solution!!!
+      $.each(response.Search, (i,v) => {
+         $("#movie-select").append(`<option value=${response.Search[i]}>${response.Search[i].Title}</option>`)
+       })
+     }).fail(() => {
+       console.log("Request failed!")
+     })
+
+   })
+
+ })
