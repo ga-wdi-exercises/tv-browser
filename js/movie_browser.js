ae14d8f94b7a4792159cc8e06e324c8a7b22d374
jared edwards <jredwards519@gmail.com>
2017-03-09 08:44:09 -0500

    initial and last commit

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,44 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+$('#search').submit(e =>{
+  e.preventDefault();
+  let input = $('#movie-search').val()
+  //clear here?
+  searchForMovie(input)
+});
+
+function searchForMovie(input){
+  $.ajax({
+    url: `http://www.omdbapi.com/?s=${input}`,
+    type: 'GET',
+    dataType: 'json',
+  }).done(response => {
+    select(input, response)
+  })
+}
+
+function select(input, response){
+  let results = response.Search
+
+  let movie = $('#movie-select')
+  movie.empty()
+  movie.append(`<option value="">Movies that match "${input}"</option>`)
+  results.forEach(movieN => {
+    movie.append(`<option value = "${movieN.imdbID}">${movieN.Title}</option>`)
+  })
+  movie.show()
+}
+
+$('#movie-select').hide().change(function() {
+  $.ajax({
+    url: `http://www.omdbapi.com/?i=${this.value}`,
+    type: 'GET',
+    dataType: 'json',
+  }).done(response => {
+    $('#movie-detail').empty()
+      .append(`<h2>${response.Title}</h2>`)
+      .append(`<img src="${response.Poster}">`)
+  })
+
+})
