ba677f7e3c70a0ab3b1dc6b46bd462b8728df0c6
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-08 21:28:41 -0500

    app bootstrapped

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,15 +1,4 @@
 // API Docs at:
 // http://www.omdbapi.com
 
-$(document).ready(()=> {
-
-  $("#movie-select").hide();
-
-  $("#submit").on("click", ()=> {
-    event.preventDefault()
-
-    let search = $("#movie-search").val
-    let url = "http://www.omdbapi.com/?="
-  })
-
-})
+angular.module("movieBrowser", [])
