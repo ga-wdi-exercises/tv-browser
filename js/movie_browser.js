0e11e335b2db967e2e51a56eb739b4d8cd37751a
Amilcar Sylvester <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-03-08 23:31:07 -0500

    moviebrowser

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,27 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+$('#movie-select').hide().on('change', function() {
+  show(this.value);
+});
+
+function movieBrowserFunction() {
+
+  var search = $("#movie-search").val()
+  var url = `http://www.omdbapi.com/?s=${search}`
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+    // $.ajax takes an object as an argument with at least three key-value pairs...
+    // (1) The URL endpoint for the JSON object.
+    // (2) Type of HTTP request.
+    // (3) Datatype. Usually JSON.
+  }).done(() => {
+    console.log("Ajax success")
+  }).fail(() => {
+    console.log("Ajax fails")
+  }).always(() => {
+    console.log("This always happens regardless.")
+  })
+}
