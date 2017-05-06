cd547e203a76f1dd92ff0f0d18707dadde34189e
meagdoh <meagdoherty@gmail.com>
2017-05-06 16:01:13 -0400

    Title list

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,36 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+$("button").on("click", (event) => {
+  // Make sure to add your API key to the URL!
+  event.preventDefault();
+  var search = $('#movie-search').val()
+  var url = "http://www.omdbapi.com/?s=" +escape(search)
+
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+    // $.ajax takes an object as an argument with at least three key-value pairs...
+    // (1) The URL endpoint for the JSON object.
+    // (2) Type of HTTP request.
+    // (3) Datatype. Usually JSON.
+  }).done((response) => {
+    var titles = []
+    data = response.Search
+
+    function getTitle() {
+      for(var i = 0; i < data.length; i++) {
+        var newTitle = data[i].Title
+        titles.push(newTitle)
+      }
+      console.log(titles)
+    }
+    getTitle()
+
+  }).fail(() => {
+    console.log("Ajax request fails!")
+  }).always(() => {
+    console.log("This always happens regardless of successful ajax request or not.")
+  })
+
+})
