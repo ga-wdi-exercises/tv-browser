957d91c6ddaa3fde2788273ef812faae30f1f34b
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-05-06 20:48:18 -0400

    add done function

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -2,5 +2,12 @@
 // http://www.omdbapi.com
 
 $("#search input[type='submit']").on('click', () => {
-  
+  var url = 'http://www.omdbapi.com/?'
+  $.ajax({
+    url: url,
+    type: 'get',
+    dataType: 'json'
+  }).done(() => {
+    console.log("Ajax request success!")
+  })
 })
