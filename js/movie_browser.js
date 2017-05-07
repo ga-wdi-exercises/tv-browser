80fbdad00de26e17258a841f38d9e818aa6e4544
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-05-07 03:14:37 -0400

    append selected titles to details div

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -4,8 +4,8 @@
 $("#submit").on('click', () => {
   event.preventDefault()
 
-  var input = $("input").val()
-  var url = "http://www.omdbapi.com/?s=" + input;
+  let keyword = $("input").val()
+  let url = "http://www.omdbapi.com/?s=" + keyword;
   $.ajax({
     url: url,
     type: "get",
@@ -17,13 +17,18 @@ $("#submit").on('click', () => {
 
     $("#movie-select").empty()
 
+    $("#movie-select").append(`<option>Movies that include "${keyword}"...</option>`)
     if (results === undefined){
       $("#movie-select").css("display", "none")
     } else {
-      results.forEach((movie) => {
+      results.forEach((movie, i) => {
         $("#movie-select").append(`<option>${movie.Title}</option>`)
       })
     }
+  $("#movie-select").change(() => {
+    let movieDetails = $("#movie-select").find(':selected').html();
+    $("#movie-detail").html(`<h2>${movieDetails}</h2>`)
+  })
 
   }).fail(() => {
     console.log("Ajax request failed!")
