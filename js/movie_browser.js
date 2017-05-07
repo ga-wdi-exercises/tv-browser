ad7cd363f27b3450f1e55b60373fab90a273cd82
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-05-06 22:33:06 -0400

    read input value

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,13 +1,21 @@
 // API Docs at:
 // http://www.omdbapi.com
 
-$("#search input[type='submit']").on('click', () => {
-  var url = 'http://www.omdbapi.com/?'
+$("#submit").on('click', () => {
+  event.preventDefault()
+
+  var input = $("input").val()
+  var url = "http://www.omdbapi.com/?t=" + input;
   $.ajax({
     url: url,
-    type: 'get',
-    dataType: 'json'
-  }).done(() => {
-    console.log("Ajax request success!")
+    type: "get",
+    dataType: "json"
+  }).done((response) => {
+    $("#movie-select").css("display", "block")
+    if (response.Response === "False"){
+      $("#movie-select").css("display", "none")
+    }
+  }).fail(() => {
+    console.log("Ajax request failed!")
   })
 })
