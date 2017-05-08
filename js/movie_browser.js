e6946a0da4366079be181ee043ace26db55a89ce
Marcus Linn Miller Jr <marcusmiller220@gmail.com>
2017-05-08 18:32:18 -0400

    added id's and more js

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,18 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+$("#submit").on('click', () => {
+    event.preventDefault()
+
+    var input = $("movie-search").val()
+    var url = 'http://www.omdbapi.com/?' + input;
+    $.ajax({
+        url: url,
+        type: 'get',
+        dataType: 'json'
+    }).done(() =>{
+        
+        }
+    }).fail(()=>{
+
+    })
+})
