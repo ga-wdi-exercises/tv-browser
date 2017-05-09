efa2c16d6a3d3eac709162b6fc8add9bbe4b9c5f
Caroline Chen <carolinecaroline@gmail.com>
2017-05-09 10:01:43 -0400

    restructure code so not just pulling value of text from the dropdown list

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -2,7 +2,7 @@ var movieSearch = $("#movie-search");
 var movieSelect = $("#movie-select");
 var firstOption = $("#first-option");
 var movieDetail = $("#movie-detail");
-
+var searchresults = []
 movieSelect.hide();
 
 $(".submit").on("click", (event) => {
@@ -18,9 +18,13 @@ $(".submit").on("click", (event) => {
 	}).done((response) => {
 		firstOption.text("Movies matching " + selection + ":")
 		for(var i=0; i < response.Search.length; i++){
+		searchresults.push([response.Search[i].Title, response.Search[i].Poster])	
 		movieSelect.append("<option>" + response.Search[i].Title + "</option>")
-		movieDetail.append("<div>" + "<img src=" + response.Search[i].Poster + "/>" + "</div>")
+		// movieDetail.append("<div>" + "<img src=" + response.Search[i].Poster + "/>" + "</div>")
+
 		}
+		console.log("search results are" + searchresults)
+		console.log("the first result is" + searchresults[0])
 		console.log("Ajax request success!")
 	}).fail(() => {
 		console.log("Ajax request fails!")
@@ -28,15 +32,18 @@ $(".submit").on("click", (event) => {
 		console.log("This always happens regardless of successful ajax request or not.")
 	})
 
+
+
 	movieSelect.change(function(){
-    var title = "";
-    $( "select option:selected" ).each(function() {
-      title = $( this ).text();
-    });
-    movieDetail.text( title );
+  	let listSelection = movieSelect.val()
+  	console.log("listSelection is" + listSelection)
+  	
+    movieDetail.text( searchresults[0][0] );
+    movieDetail.append("<div>" + "<img src=" + searchresults[0][1]+ "/>" + "</div>");
   })
  
 
 })
+
 
 
