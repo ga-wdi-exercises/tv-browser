31b12f840e00bc774edb7656ec6086aca316e14a
Curtis Ostle <curtisostle@gmail.com>
2017-05-09 08:33:13 -0400

    string interpolated user input

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,25 @@
-// API Docs at: 
-// http://www.omdbapi.com
+console.log('connected')
+let movieSelect = $('#movie-select')
+let searchText = $('input#movie-search');
+let button = $('#button');
+
+  button.on("click", (evt) => {
+    evt.preventDefault();
+    console.log(searchText.val());
+
+    let url = `http://www.omdbapi.com/?s=${searchText.val()}`
+    console.log(url);
+    $.ajax({
+      url: url,
+      type: "get",
+      dataType: "json"
+    }).done((response) => {
+      response.Search.map ((movie) => {
+          console.log(movie.title)
+      });
+    }).fail((response) => {
+      console.log("Ajax request fails!")
+    }).always(() => {
+      console.log("This always happens regardless of successful ajax request or not.")
+    })
+  })
