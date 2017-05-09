bb90af32bc75c384e23cdff1a05cb329569640b1
Curtis Ostle <curtisostle@gmail.com>
2017-05-09 13:21:03 -0400

    all working except hitting submit on another search throws error

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,22 +1,39 @@
-console.log('connected')
 let movieSelect = $('#movie-select')
 let searchText = $('input#movie-search');
 let button = $('#button');
+let firstOption = $('#firstOption');
+let showDetail = $('#movie-detail')
+
+movieSelect.hide();
 
   button.on("click", (evt) => {
     evt.preventDefault();
-    console.log(searchText.val());
+    searchText = searchText.val();
+    movieSelect.show()
+      showDetail.empty();
+      movieSelect.empty();
+      movieSelect.append(`<option>Movies matching \'${searchText}\'</option>`)
 
-    let url = `http://www.omdbapi.com/?s=${searchText.val()}`
-    console.log(url);
+    let url = `http://www.omdbapi.com/?s=${searchText}`
+
     $.ajax({
       url: url,
       type: "get",
       dataType: "json"
     }).done((response) => {
+      let movies = [];
       response.Search.map ((movie) => {
-          console.log(movie.title)
-      });
+        movies.push(movie);
+        movieSelect.append(`<option>${movie.Title}</option>`)
+      })
+      movieSelect.change(() => {
+      showDetail.empty();
+      let chosenMovie = movies.find((movie) => {
+        return movieSelect.val() === movie.Title;
+      })
+      showDetail.append(`<h1>${chosenMovie.Title}</h1>`)
+      showDetail.append(`<img src=${chosenMovie.Poster}>`)
+      })
     }).fail((response) => {
       console.log("Ajax request fails!")
     }).always(() => {
