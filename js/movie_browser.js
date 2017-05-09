826aab197d899a29fec23862358e557306794882
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-05-08 22:51:29 -0400

    inital commit

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,18 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+$("#submit").on("click", () => {
+    var searchMovie = $("#movie-search").val()
+    var url = "http://www.omdbapi.com/?s=" +escape(searchMovie);
+
+  $.ajax({
+      url: url,
+      type: "get",
+      dataType: "json"
+  }).done(() => {
+    console.log("Ajax request success!")
+  }).fail(() => {
+    console.log("Ajax request fails!")
+  })
+})
+
+$("#movie-select").hide();
