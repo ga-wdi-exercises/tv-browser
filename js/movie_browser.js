c4eb45dde952737d5eb668d0edb7266a5f9a187c
Jeong Hoon Koo <junghoone17@gmail.com>
2017-05-08 23:59:19 -0400

    added function to search and show title in the dropdown bar

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,37 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+$("#movie-select").hide();
+
+$("#movie-search-button").on("click", (event) => {
+  event.preventDefault();
+  let keyword = $("#movie-search").val();
+  let url = "http://www.omdbapi.com/?s=" + keyword
+  $("#movie-select").show();
+  $("#movie-select").empty();
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+  }).done((response) => {
+    $("#movie-select").append(`<option> Movies matching "${keyword}"...</option>`)
+    searchAll(response)
+
+
+  }).fail(() => {
+   console.log("Ajax request fails!")
+  }).always(() => {
+    // console.log(url)
+  })
+})
+
+function searchAll (response){
+  for (i = 0; i < response.Search.length; i++) {
+    let title = response.Search[i].Title;
+    $("#movie-select").append(`<option> "${title}" </option>`);
+  }
+}
+
+function showDetail (selected){
+
+}
