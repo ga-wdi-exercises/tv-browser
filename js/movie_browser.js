e30c5cd27af18fd24b3f4fd18ac165b40eff8c23
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-05-08 20:08:54 -0400

    setting up router and eidted index with ui-view

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,20 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+angular
+  .module(moviebrowser, [
+    "ui.router"
+  ])
+  .config([
+    "stateProvider", Router
+  ])
+
+function Router($stateProvider) {
+  $stateProvider
+  .state("index", {
+    url: "/",
+    controller: "IndexController",
+    controllerAs: "vm",
+    templateUrl: "js/ng-views/index.html"
+  })
+}
