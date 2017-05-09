bd98f5b6ed6f14c50237ef77c227d267a05953a8
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-05-08 21:45:34 -0400

    rewrite all of code because i was confused about requirments

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,20 +1,32 @@
-// API Docs at:
-// http://www.omdbapi.com
 
-angular
-  .module(moviebrowser, [
-    "ui.router"
-  ])
-  .config([
-    "stateProvider", Router
-  ])
+$(document).ready(() => {
+   $("#movie-select").hide();
 
-function Router($stateProvider) {
-  $stateProvider
-  .state("index", {
-    url: "/",
-    controller: "IndexController",
-    controllerAs: "vm",
-    templateUrl: "js/ng-views/index.html"
-  })
-}
+   $(".find").on("click", () => {
+     $("#movie-select").show();
+      event.preventDefault()
+
+      let search = $("#movie-search").val()
+      let url = `http://www.omdbapi.com/?s=${search}`
+
+      $("#movie-select").empty();
+
+      $.ajax({
+         url: url,
+         type: "get",
+         dataType: "json"
+       }).done((response) => {
+         console.log("After search movies")
+      $("#movie-select").append(`<option> Movies that matches with the keyword: "${search}"...</option>`)
+
+         //had to look at the solution!!!
+      $.each(response.Search, (i,v) => {
+         $("#movie-select").append(`<option value=${response.Search[i]}>${response.Search[i].Title}</option>`)
+       })
+     }).fail(() => {
+       console.log("Request failed!")
+     })
+
+   })
+
+ })
