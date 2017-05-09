9315642be3ca2e8bf8b8edb044b66c70846c2c1a
Erin <erinedawson@gmail.com>
2017-05-08 22:16:49 -0400

    add click event on submit.

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -2,4 +2,21 @@
 // http://www.omdbapi.com
 
 angular
-  .module("movieBrowser", []);
+  .module("movieBrowser", [])
+
+  .controller("MoviesController", [
+    MoviesControllerFunction
+  ])
+
+function MoviesControllerFunction () {
+
+}
+$('#movie-select').hide().on('change', function() {
+  show(this.value);
+});
+
+$.ajax ({
+  url: "http://www.omdbapi.com/?",
+  type: "get",
+  dataType: "json"
+})
