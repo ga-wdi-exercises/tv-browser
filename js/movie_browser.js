d01f56f1d681cecd526838b6612354e4c8bf38c2
Omar Chaudhry <omarchaudhry.1991@gmail.com>
2017-05-10 11:56:07 -0400

    123

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,16 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
++$("submit").on("click",function(){
+ +var url ="http://www.omdbapi.com/"
+ +$.ajax({
+ +  url:"/movie",
+ +  type:"GET",
+ +  dataType:"json"
+ +}).done(() => {
+ +    console.log("Request Successful")
+ +  }).fail(() => {
+ +    console.log("Request Failed")
+ +  }).always(() => {
+ +    console.log("This message occurs for every request")
+ +  })
+ +})
