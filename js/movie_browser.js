3c245e2917118045b1fc0ef0eb9884cd45e52711
amlak befekadu <amlakgirma@yahoo.com>
2017-05-10 06:46:01 -0400

    movie

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,16 @@
-// API Docs at: 
-// http://www.omdbapi.com
+// API Docs at:
+// http://www.omdbapi.com<head>
+$("button").on("click",function(){
+var url ="http://www.omdbapi.com/"
+$.ajax({
+  url:"/movie",
+  type:"GET",
+  dataType:"json"
+}).done(() => {
+    console.log("Ajax request success!")
+  }).fail(() => {
+    console.log("Ajax request fails!")
+  }).always(() => {
+    console.log("This always happens regardless of successful ajax request or not.")
+  })
+})
