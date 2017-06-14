c1611e7c0f79b11711c1094bf492972de83df00b
superbuggy <jpqreichard@gmail.com>
2017-06-14 16:23:16 -0400

    update to js standard formatting

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,52 +1,35 @@
 angular
-.module("mb",[
-  "ui.router"
-])
-.config([
-  "$stateProvider",
-  Router
-])
-.controller("IndexController",[
-  IndexController
-])
+  .module('mb', [
+    'ui.router'
+  ])
+  .config([
+    '$stateProvider',
+    Router
+  ])
+  .controller('IndexController', [
+    IndexController
+  ])
 
-function IndexController(){
+function IndexController () {
   var vm = this
-  vm.searchTerm = "up"
-  vm.search = function(){
-    this.$apply(function(){
-
-
-    $.getJSON(`http://www.omdbapi.com/?s=${vm.searchTerm}`,function(response){
-      console.log("finished searchin")
-      vm.movies = response.Search
-      console.log(vm)
-    })
+  vm.searchTerm = 'up'
+  vm.search = function () {
+    this.$apply(function () {
+      $.getJSON(`http://www.omdbapi.com/?s=${vm.searchTerm}`, function (response) {
+        console.log('finished searchin')
+        vm.movies = response.Search
+        console.log(vm)
       })
+    })
   }
 }
 
-function Router($stateProvider){
+function Router ($stateProvider) {
   $stateProvider
-  .state("index",{
+  .state('index', {
     url: '/',
-    controller:'IndexController',
-    controllerAs:'vm',
+    controller: 'IndexController',
+    controllerAs: 'vm',
     templateUrl: 'js/ng-views/index.html'
   })
 }
-
-// API Docs at:
-// http://www.omdbapi.com
-
-// bootstrap the app
-
-// set up a router
-
-// set up a controller
-
-// some template html
-
-// ng-submit on the form
-
-// request list of movies from api
