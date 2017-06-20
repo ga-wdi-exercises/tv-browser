526365c6a8c523ade1fd4e49517f394bbaa3d1d0
Tim Chase <tchase4544@gmail.com>
2017-06-20 14:33:12 -0400

    progess at lunch

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,59 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+$(document).ready(function(){
+	// $('#show-select').hide()
+	let input = $('#show-search')
+	let button = $('#submit')
+
+	button.click((e)=>{
+		e.preventDefault()
+		clearMenu()
+		queryData()
+		showDiv()
+	})
+
+	function showDiv(){
+		$('#show-select').show()
+	}
+
+	function queryData() {
+		$.getJSON( "http://api.tvmaze.com/search/shows?q="+input.val(), function( datas ) {
+    			console.log(datas);
+    			let data = datas
+    			popList(data)
+    			popDetail(data)
+    		})
+	}
+	function popList(data) {
+		for (let i = 0; i<data.length;i++){
+			$('#show-select').append(`<option value="${i}">${data[i].show.name}</option>`)
+		}
+
+	}
+ 	function clearMenu() {
+ 		$('#show-select > *').remove()
+ 	}
+
+ 	function popDetail(data) {
+ 		
+ 		$('#show-select').change(function () {
+			let text = "";
+			$("option:selected").each(function() {
+				let sel = $("option:selected").val()
+				text = data[sel].show.summary
+				//name
+				//image
+			})
+			$( "#show-detail" ).text( text )
+		})
+ 	}
+
+})
+
+	
+
+
+// http://api.tvmaze.com/search/shows?q=
+//ajax call
+// ('#show-select').append(`<option>${data.show.name}</option>`)
