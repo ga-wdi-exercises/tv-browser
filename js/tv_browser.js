7671675f763edb8faa62ee6a246d6d605f4e064d
Fitssum Haile <fitsum.wolde@gmail.com>
2017-06-22 08:54:58 -0400

    solution

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,25 @@
-// API Docs at:
-// http://www.tvmaze.com/api
+$( document ).ready(function() {
+
+  $("#submitButton").click(function(e) {
+      e.preventDefault()
+      $("#show-select").show();
+    });
+
+  $("#submitButton").click(function() {
+    var requestData = $("#show-select").val();
+
+
+    $.ajax({
+      url: "http://api.tvmaze.com/search/shows?q=" + requestData, function showData(){
+        for (let i = 0; i < data.length; i++) {
+          $('#show-select').append(`<option value="${i}">${[i]show.name}</option>`)
+        }
+      }
+      method: "get",
+      dataType: "json",
+      done: function() {
+        $("#show-select").html(dataDetail());
+      }
+    })
+  })
+});
