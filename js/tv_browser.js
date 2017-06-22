f8b2e0144d86dbdcd01e562c6fd68aee1416311c
tedlayea <tedlayea@gMAIL.com>
2017-06-22 08:55:39 -0400

    tv-show

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,36 @@
 // API Docs at:
-// http://www.tvmaze.com/api
+// // http://www.tvmaze.com/api
+
+
+angular
+.module("tv_show",[])
+.controller("control",ControlFunction)
+
+function ControlFunction (){
+  console.log("Am the controller!")
+  $("#show-select").hide()
+}
+
+// function serachButton(){
+//   console.log("clicked");
+// }
+
+$("#button").on("click", () => {
+  console.log("clicked");
+  $("#show-select").show()
+  // var keyword=$("show-select").val()
+  var keyword=this.keyword
+  console.log(keyword);
+
+  var url = "http://www.tvmaze.com/search?q="+keyword+".json"
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+  }).done((response) => {
+    console.log("Ajax request success!")
+    console.log(response);
+  }).fail(() => {
+    console.log("Ajax request fails!")
+  })
+})
