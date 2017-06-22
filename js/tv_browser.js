826ab32c5f2a37328736e4d2ee7d058ad7af4c1c
ANTWANA WADLINGTON <antwana.wadlington@gmail.com>
2017-06-22 08:46:03 -0400

    Initial Commit

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,47 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+angular
+  .module("tv", []);
+
+var url = "https://api.wunderground.com/api/your_key/geolookup/conditions/q/va/midlothian.json"
+
+
+
+
+
+$(document).ready(function()
+{
+ $("#show-select").change(function()
+ {
+
+  if($(this).val() == "")
+  {
+   $("#show-select").hide();
+  }
+  else
+  {
+   $("#show-select").show();
+   $.getJSON("http://api.tvmaze.com/schedule")
+  }
+ });
+  $("#show-select").show();
+
+$("#search").submit(function(event){
+  alert("Submit Works");
+});
+
+});
+
+
+
+// $(document).ready(function(){
+//   $("#show-select").change(function){
+//     if ($(this).val() == " "){
+//       $("#show-select").hide();
+//     }
+//     else{
+//       $("#show-select").show();
+//     }
+//   })
+// });
