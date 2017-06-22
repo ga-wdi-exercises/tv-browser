3bc574db4b0d07481c5e6cabfc2b006ada64da2b
Robert Lowe <RobLowe1992@Yahoo.com>
2017-06-22 07:41:12 -0400

    Completed

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,46 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+
+
+    let showSelect = $('#show-select');
+    let showDetail = $('#show-detail');
+    const submit = $('input[type="submit"]')
+    const search = $('input[type="search"]')
+
+    showSelect.hide()
+
+    submit.click(function(e){
+        e.preventDefault();
+        showSelect.show()
+        query();
+    });
+
+    function query(){
+        $.getJSON(`http://api.tvmaze.com/search/shows?q=${search.val()}`, function(res){
+            console.log(res);
+            appendResults(res);
+            appendDetail(res)
+        })
+    }
+
+    function appendResults(res){
+        res.forEach(function(show, index){
+            showSelect.append(`<option data-id="${index}">${show.show.name}</option>`)
+        })
+    }
+
+    function appendDetail(res) {
+
+        $('#show-select').change(function () {
+            let detail = "";
+            $("option:selected").each(function() {
+                let showId = $("option:selected").attr('data-id')
+                console.log(showId)
+                detail = `<img src="${res[showId].show.image.medium}"><br><h2>${res[showId].show.name}</h2><p>${res[showId].show.summary}</p>`
+            })
+            $("#show-detail").html(detail)
+        })
+    }
+
+
