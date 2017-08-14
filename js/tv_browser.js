84f1658e20afa841b268488f85951fdf7f36e9b8
Perry Fustero <pjfust@gmail.com>
2017-08-14 18:44:37 -0400

    finished first 2 steps

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,23 @@
+/* global $ */
 // API Docs at:
 // http://www.tvmaze.com/api
+$(document).ready(function () {
+  $('#submit').click(function () {
+    event.preventDefault()
+    $('#show-select').toggle()
+    var search = $('#show-search').val()
+    var url = `http://api.tvmaze.com/search/shows?q=${search}`
+    $.ajax({
+      url: url,
+      type: 'get',
+      dataType: 'json'
+    }).done((response) => {
+      $('#top-option').text(`shows matching ${search}`)
+      for (let i = 0; i < response.length; i++) {
+        $('#show-select').append(`<option>${response[i].show.name}</option>`)
+      }
+    }).fail(() => {
+      console.log('Ajax request fails!')
+    })
+  })
+})
