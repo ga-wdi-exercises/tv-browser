ff2554fc07d77df503db636df05d4105cd0b0ca4
MATT FILBERT <mcfilbert@email.wm.edu>
2017-08-14 19:27:24 -0400

    frequent commits are crucial!

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,35 @@
+
+/* global $ */
+/* global event */
 // API Docs at:
 // http://www.tvmaze.com/api
+$(document).ready(() => {
+  $('#submit').click(function () {
+    event.preventDefault()
+    $('.new').remove()
+    var search = $('#show-search').val()
+    var url = `http://api.tvmaze.com/search/shows?q=${search}`
+    $('.select').text(`shows matching ${search.toLowerCase()}...`)
+    $('#show-select').show()
+    $.ajax({
+      url: url,
+      type: 'get',
+      dataType: 'json'
+    }).done((response) => {
+      for (var i = 0; i < response.length; i++) {
+        $('#show-select').append(`<option class='new'>${response[i].show.name}</option>`)
+      }
+      $('#show-select').change(function () {
+        $('#show-detail').empty()
+        var index = $('#show-select').prop('selectedIndex') - 1
+        $('#show-detail').append(`<h2 class='info'>${response[index].show.name}<h2>`)
+        $('#show-detail').append(`<img src="${response[index].show.image.medium}" alt="Image of ${response[index].show.name}">`)
+        $('#show-detail').append(`<p class='info'>${response[index].show.summary}</p>`)
+      })
+    }).fail(() => {
+      console.log('BOO')
+    }).always(() => {
+      console.log('EVERYTIME')
+    })
+  })
+})
