4a2b1748eaa4c783fc0a9329401ae3d7664ad44b
Perry Fustero <pjfust@gmail.com>
2017-08-14 19:28:26 -0400

    pretty much done

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -4,18 +4,32 @@
 $(document).ready(function () {
   $('#submit').click(function () {
     event.preventDefault()
-    $('#show-select').toggle()
+    $('.new').remove()
+    $('#show-select').show()
     var search = $('#show-search').val()
     var url = `http://api.tvmaze.com/search/shows?q=${search}`
+
     $.ajax({
       url: url,
       type: 'get',
       dataType: 'json'
+
     }).done((response) => {
-      $('#top-option').text(`shows matching ${search}`)
+      $('#top-option').text(`shows matching ${search}...`)
       for (let i = 0; i < response.length; i++) {
-        $('#show-select').append(`<option>${response[i].show.name}</option>`)
+        $('#show-select').append(`<option class="new">${response[i].show.name}</option>`)
       }
+
+      $('#show-select').change(function () {
+        let selection = $('#show-select').prop('selectedIndex')
+        if (selection >= 0) {
+          var index = selection - 1
+        }
+        $('#show-detail').empty()
+        $('#show-detail').append(`<h2>${response[index].show.name}</h2>`)
+        $('#show-detail').append(`<img src=${response[index].show.image.medium}>`)
+        $('#show-detail').append(`<p>${response[index].show.summary}</p>`)
+      })
     }).fail(() => {
       console.log('Ajax request fails!')
     })
