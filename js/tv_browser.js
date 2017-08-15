fe9184d4c399d64a0ead928bc290e46056d6d970
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-15 12:56:00 -0400

    made json request

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,23 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+$(document).ready(() => {
+
+  let tvShow = $('#search').val()
+  let url = `http://api.tvmaze.com/search/shows?q=${tvShow}`
+
+  $('#search').on('submit', function (){
+    event.preventDefault()
+    $.ajax({
+      url: url,
+      type: 'get',
+      dataType: 'json'
+    }).done((response) => {
+      console.log(response)
+    }).fail(() => {
+      console.log('request failed')
+    }).always(() => {
+      console.log('request made')
+    })
+  })
+})
