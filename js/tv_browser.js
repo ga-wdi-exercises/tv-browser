72ac8130f98b005789222f98bcb0d9cb1eafac8c
Charles Cox <cox.charles.w@gmail.com>
2017-08-15 20:09:47 -0400

    ajax event listern set up for submit button

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,25 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+$(document).on('click', () => {
+  $('.submit').on('click', (event) => {
+    event.preventDefault()
+    // Make sure to add your API key to the URL!
+
+    var url = 'http://api.tvmaze.com/search/shows?q=girls'
+    $.ajax({
+      url: url,
+      type: 'get',
+      dataType: 'json'
+      // $.ajax takes an object as an argument with at least three key-value pairs...
+      // (1) The URL endpoint for the JSON object.
+      // (2) Type of HTTP request.
+      // (3) Datatype. Usually JSON.
+    }).done(() => {
+      console.log('Ajax request success!')
+    }).fail(() => {
+      console.log('Ajax request fails!')
+    }).always(() => {
+      console.log('This always happens regardless of successful ajax request or not.')
+    })
+  })
+})
