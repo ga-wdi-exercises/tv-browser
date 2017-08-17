7926c4522c4fbb1c92f948a36bcb63d52909d75e
Mitche Guevara <mitche.guevara@gmail.com>
2017-08-17 09:38:53 -0400

    basic functionality

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,10 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+// var url = 'http://api.tvmaze.com/'
+// $http({
+//   url: url,
+//   method: 'GET',
+//   dataType: 'json'
+// }).then (() => {
+//   console.log('success')
+// })
