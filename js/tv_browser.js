6031425be0e0dad277f5dd6e2bf7b923c276d1ee
Luis Wiese <luiswiese@gmail.com>
2017-08-18 02:00:41 -0400

    done

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -2,6 +2,7 @@
 // http://www.tvmaze.com/api
 $('.submit').on('click', () => {
   event.preventDefault()
+  // var search store the value of the search text area.
   var search = $(".search").val()
   var url = "http://api.tvmaze.com/search/shows?q=" + search
   $.ajax({
@@ -10,21 +11,25 @@ $('.submit').on('click', () => {
     dataType: 'json'
   })
   .done((response) => {
-    $('.results').remove()
     $('option').remove()
     console.log(response)
     response.forEach((item) => {
       $(`<option value="${item.show.id}">${item.show.name}</option>`).appendTo('#show-select')
     })
-
-    var option = $("#show-select").val()
-    console.log(option)
+    // $('#show-select').show() will show the option menu when press enter or click submit
     $('#show-select').show()
-    $(`<img class ='results' src=${response[1].show.image.original}>`).appendTo('main')
-    $('<div class ="results" ></div>').appendTo('main').html(`${response[1].show.summary}`)
-
+    $('#show-select').change(function () {
+      // $('.results').remove() will clean the results when click in another option
+    $('.results').remove()
+    var index = $('#show-select').prop('selectedIndex') + 0
+    // name & summary requested before show.image
+    $('#show-detail').append(`<div class='results'>${response[index].show.name}</div>`)
+    $('#show-detail').append(`<div class='results'>${response[index].show.summary}</div>`)
+    //show.image requested last, if the show has not image could cause a conflict and stop loading whatever come after
+    $('#image').append(`<img class ='results' src=${response[index].show.image.original}>`)
+  })
   }).fail(() => {
-    $('<div class="results"></div>').appendTo('main').text()
+    $('<div class="results"></div>').appendTo('main').text('Fail')
   }).always(() => {
     $('').appendTo('main').text()
     })
