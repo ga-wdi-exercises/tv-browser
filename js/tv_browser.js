28c96fcb8cbe1ab5a14345ff223417b053bbe202
Luis Wiese <luiswiese@gmail.com>
2017-08-17 22:44:02 -0400

    almost there

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,31 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+$('.submit').on('click', () => {
+  event.preventDefault()
+  var search = $(".search").val()
+  var url = "http://api.tvmaze.com/search/shows?q=" + search
+  $.ajax({
+    url: url,
+    type: 'get',
+    dataType: 'json'
+  })
+  .done((response) => {
+    $('.results').remove()
+    $('option').remove()
+    console.log(response)
+    response.forEach((item) => {
+      $(`<option value="${item.show.id}">${item.show.name}</option>`).appendTo('#show-select')
+    })
+    
+        var option = $("option").val()
+        console.log(option)
+    $('#show-select').show()
+    $(`<img class ='results' src=${response[1].show.image.original}>`).appendTo('main')
+    $('<div class ="results" ></div>').appendTo('main').text(`${response[1].show.summary}`)
+
+  }).fail(() => {
+    $('<div class="results"></div>').appendTo('main').text()
+  }).always(() => {
+    $('<div class="results"></div>').appendTo('main').text()
+    })
+  })
